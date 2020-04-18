5f79132853b038883389f0c2fe17b1addd1811ba
jamie legg <jamie@truepill.com>
2020-04-18 12:16:46 +0100

    hoist fix

--- src/common/moderator.ts
+++ src/common/moderator.ts
@@ -2,5 +2,11 @@ import { Message } from "discord.js"
 import { MODERATOR_ROLE_NAME } from "../const"
 
 export const isAuthorModerator = (message: Message):boolean => {
-    return message.member.roles.hoist.name === MODERATOR_ROLE_NAME
+   if(message.member.roles.hoist) {
+      return message.member.roles.hoist.name === MODERATOR_ROLE_NAME
+   }
+   else {
+      return false;
+   }
+    
 } 
