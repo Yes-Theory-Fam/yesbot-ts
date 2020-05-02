6db712471c47bc6808491649552e7dd9f24409dd
Håkon Solbjørg <hakon@solbj.org>
2020-05-02 02:48:14 +0200

    fix: Don't try to message channel on startup if channel is not defined

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -3,20 +3,17 @@ import bot from "../index"
 import { GUILD_ID, OUTPUT_CHANNEL_ID } from '../const';
 
 class Ready {
-
-
     bot: Discord.Client;
 
     constructor(bot: Discord.Client) {
         this.bot = bot;
         console.log(`${bot.user.tag} - Online`)
         const guild = this.bot.guilds.resolve(GUILD_ID)
-        const outputChannel = <Discord.TextChannel>guild.channels.resolve(OUTPUT_CHANNEL_ID)
-        outputChannel.send(`${bot.user.tag} - Online`)
+        if (OUTPUT_CHANNEL_ID) {
+            const outputChannel = <Discord.TextChannel>guild.channels.resolve(OUTPUT_CHANNEL_ID)
+            outputChannel.send(`${bot.user.tag} - Online`)
+        }
     }
-
-
-
 }
 
 export default Ready;
