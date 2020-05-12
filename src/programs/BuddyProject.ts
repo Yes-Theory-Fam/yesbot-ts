d25ce5ccd00ad530d5c928bb01c528b0d88cf6af
jamie-legg <legg.jamie@gmail.com>
2020-05-12 03:11:55 +0100

    omw

--- src/programs/BuddyProject.ts
+++ src/programs/BuddyProject.ts
@@ -1,72 +1,49 @@
-import firebase from "firebase";
 import { db } from "..";
 import { GuildMember, PartialGuildMember } from "discord.js";
-
-interface BuddyProjectSignup {
-  id: string,
-  username: string,
-  displayName: string
-}
+import { BuddyProjectEntryRepository, BuddyProjectEntry } from "../entities/BuddyProjectEntry";
 
 
-export async function BuddyProjectSignup(member:GuildMember | PartialGuildMember) {
+
+export async function BuddyProjectSignup(member:GuildMember | PartialGuildMember):Promise<null> {
+  
+  const discord_user = new Date(member.joinedAt.toDateString()) < new Date(new Date().toDateString());
 
   const dmChannel = await member.createDM();
 
-  if (db === null) {
-    console.error("The database needs to be initialized first!");
-    throw new Error("The database needs to be initialized first!");
+  const buddyEntries = await BuddyProjectEntryRepository();
+
+  const hasEntered = await buddyEntries.findOne(member.id);
+
+  if(hasEntered) {
+    if(hasEntered.matched) {
+
+    }
+    else {
+
+    }
+    dmChannel.send(hasEntered.matched ?
+      `It looks like I already found you a match! Did <@${hasEntered.buddy_id}> stopped replying? :grin:`:
+      "Hey there, it looks like you just tried to sign up to the Buddy Project again, no need to do that, you're already registered!" 
+      )
   }
+  else {
 
-  const { displayName } = member;
-  const { id, username} = member.user;
+    const buddyEntry = await buddyEntries.save({
+      "user_id":member.id,
+      "matched":false,
+      discord_user,
+    })
 
-  // Let's create a b64 of the discord userid (which won't change)
-  // and use that as the identifier in firebase
-
-  const firebaseUserId = Buffer.from(id).toString('base64');
+    const successMessage = "Woo! You just signed up to the buddy project, exciting right? I'll message you again in a week or so with your buddy and what you need to do next!";
+    dmChannel.send(successMessage)
+   
+    const potentialMatch = await buddyEntries.find({where: { discord_user: !discord_user}})
+  }
   
-  const buddyDoc = await db.store
-    .collection("buddyproject")
-    .doc(firebaseUserId)
-    .get();
-
-  const existsMessage = "Hey there, it looks like you just tried to sign up to the Buddy Project again, no need to do that, you're already registered!";
-  const successMessage = "Woo! You just signed up to the buddy project, exciting right? I'll message you again in a week or so with your buddy and what you need to do next!";
-
-  dmChannel.send( buddyDoc.exists? existsMessage : successMessage)
-  if(buddyDoc.exists) return;
-
-  const buddy: BuddyProjectSignup = {
-    id,
-    username,
-    displayName,
-  };
-
-  db.store.collection("buddyproject").doc(firebaseUserId).set(buddy);
-  return true;
+  
+  return null;
 }
 
-export async function fetchBuddyProjectSignup(
-  discordUserId: string
-): Promise<BuddyProjectSignup | null> {
-  if (db.store === null) {
-    console.error("The database needs to be initialized first!");
-    return null;
-    // return reject("The database needs to be initialized first!");
-  }
+export const BuddyProjectMatch = (buddyEntry:BuddyProjectEntry, member :GuildMember | PartialGuildMember ) => {
 
-  // Let's create a b64 of the discord userid (which won't change)
-  // and use that as the identifier in firebase
-  const firebaseUserId = btoa(discordUserId);
-
-  const signup = await db.store.collection("buddyproject").doc(firebaseUserId).get();
-  if (signup.exists) {
-    const data = signup.data();
-    if (data !== null && data !== undefined) {
-      return data as BuddyProjectSignup;
-    }
-  }
-
-  return null;
 }
