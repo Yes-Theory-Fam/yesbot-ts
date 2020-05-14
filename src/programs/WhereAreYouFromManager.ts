a45b93c7d92745c376d125ad1149281b98faf9fc
geisterfurz007 <michelvonv@me.com>
2020-05-14 19:47:10 +0200

    fix: instructions for a new user

--- src/programs/WhereAreYouFromManager.ts
+++ src/programs/WhereAreYouFromManager.ts
@@ -54,7 +54,7 @@ export default async function WhereAreYouFromManager(pMessage: Discord.Message) 
             pMessage.member.createDM().then(dmChannel => {
                 const rules = pMessage.guild.channels.cache.find(c => c.name === "rules");
                 const generalInfo = pMessage.guild.channels.cache.find(c => c.name === "general-info")
-                dmChannel.send(`Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIt would be awesome to know your name, could you reply to this message with your first name please? Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`)
+                dmChannel.send(`Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIf you'd like me to change your name on the server for you, just drop me a message and I will help you out! Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`)
             })
         }
     }
