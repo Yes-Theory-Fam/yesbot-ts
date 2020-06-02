73b8ecd47ded12aeb672b4b52f78987d0462168d
jamie-legg <legg.jamie@gmail.com>
2020-06-02 18:23:29 +0100

    welcome messages

--- src/programs/WhereAreYouFromManager.ts
+++ src/programs/WhereAreYouFromManager.ts
@@ -1,4 +1,4 @@
-import Discord, { TextChannel } from 'discord.js';
+import Discord, { TextChannel, User } from 'discord.js';
 import Tools from '../common/tools';
 import {isRegistered, textLog} from '../common/moderator';
 import { Country, countries } from "../collections/flagEmojis";
@@ -54,9 +54,32 @@ export default async function WhereAreYouFromManager(pMessage: Discord.Message) 
             pMessage.member.createDM().then(dmChannel => {
                 const rules = pMessage.guild.channels.cache.find(c => c.name === "rules");
                 const generalInfo = pMessage.guild.channels.cache.find(c => c.name === "general-info")
+                const welcomeChat = <TextChannel>pMessage.guild.channels.cache.find(c => c.name === "welcome-chat");
+                welcomeChat.send(getWelcomeMessage(pMessage.member.user))
                 dmChannel.send(`Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIf you'd like me to change your name on the server for you, just drop me a message and I will help you out! Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`)
             })
         }
     }
 
 }
+
+const getWelcomeMessage = (user:User) => {
+    const welcomeMessages = [
+        `${user.toString()}> just joined the party! Make some noise everyone :zany_face:`,
+        `${user.toString()}> just *sliiiiid* into the server.`,
+        `Everyone welcome ${user.toString()}> !`,
+        `Welcome ${user.toString()}, say hi! `,
+        `Glad you're finally here ${user.toString()}, we've been waiting for you :heart_eyes: `,
+        `Welcome ${user.toString()}. We hope you brought :pizza: `,
+        `${user.toString()} just hopped in :rabbit2: What a lovely day! `,
+        `Yayyyy, ${user.toString()} made it!`,
+        `${user.toString()} just showed up, make some space for them! `,
+        `${user.toString()} is here :tada:`,
+        `${user.toString()} just landed :rocket:`
+        ];
+    return welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
+
+    
+    
+}
+
