88acc96d995603580a420c2ad5201ffb8bafbbb9
Dmitri Panfilov <dimpanf@outlook.com>
2020-09-03 19:27:07 +0200

    oops

--- src/events/GuildMemberUpdate.ts
+++ src/events/GuildMemberUpdate.ts
@@ -54,7 +54,7 @@ class GuildMemberUpdate {
     );
     if (
       nitroColor &&
-      !hasRole(newMember, "Nitro Booster") &&
+      !hasRole(newMember, "Nitro Booster") ||
       !hasRole(newMember, "Support")
     ) {
       newMember.roles.remove(nitroColor);
