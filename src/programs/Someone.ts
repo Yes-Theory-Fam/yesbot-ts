0873dabfa11b2aead78cd6f90bd5a17da6dbec79
Travis <traviscanbefound@gmail.com>
2020-09-16 22:24:42 +0800

    someone loophole test fix17 revert

--- src/programs/Someone.ts
+++ src/programs/Someone.ts
@@ -35,14 +35,10 @@ async function Someone(message: Message) {
 
   const words = Tools.stringToWords(message.content);
   const arg = words[1];
-
+  if (arg.includes("@"))
+    message.channel.send(`Unknown argument "${arg}". Did you mean "online"?`);
   if (arg && arg != "online")
-    message.channel.send(
-      `Unknown argument "${arg}". im update. Did you mean "online"?`,
-      {
-        disableMentions: "everyone",
-      }
-    );
+    message.channel.send(`Unknown argument "${arg}". Did you mean "online"?`);
   else {
     const { member } = message;
     const target = await getTarget(arg, message);
