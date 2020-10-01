ae3b59fe650bdc4894e023f30fab14a06f561c1e
geisterfurz007 <michelvonv@me.com>
2020-10-01 22:25:06 +0200

    feat: inktober !topic in visual-design

--- src/programs/TopicManager.ts
+++ src/programs/TopicManager.ts
@@ -5,6 +5,8 @@ const QUESTION_LINK: string =
   "https://spreadsheets.google.com/feeds/cells/1xUIqCaSrjyQzJeJfnXR0Hix6mDkaFhVauFmJb8Pzkj0/1/public/full?alt=json";
 const MAKEUP_CHALLENGE_PICTURE_URL =
   "https://cdn.discordapp.com/attachments/698545400075780147/712491981158481940/image0.png";
+const INKTOBER_IMAGE_URL =
+  "https://media.discordapp.net/attachments/689589403189772291/761243856946987078/2020promptlist.png";
 
 export default async function TopicManager(message: Message) {
   const channel: TextChannel = <TextChannel>message.channel;
@@ -27,6 +29,11 @@ export default async function TopicManager(message: Message) {
     case "beauty-and-fashion":
       const image = new MessageAttachment(MAKEUP_CHALLENGE_PICTURE_URL);
       message.channel.send(image);
+      break;
+
+    case "visual-design":
+      message.channel.send(new MessageAttachment(INKTOBER_IMAGE_URL));
+      break;
 
     default:
       break;
