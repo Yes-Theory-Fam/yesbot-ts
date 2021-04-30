0ac0074a6d9715dc556d10648746d11171b199f4
hlatch <hassoune80@gmail.com>
2021-04-30 19:43:42 +0200

    Update GroupManager.ts

--- src/programs/GroupManager.ts
+++ src/programs/GroupManager.ts
@@ -296,104 +296,98 @@ const searchGroup = async (
   requestedGroupName: string = ""
 ) => {
   const groupRepository = await UserGroupRepository();
-  const group = await groupRepository.findOne({
-    where: {
-      name: requestedGroupName,
-    },
-  });
+  const groupsPerPage = 4;
+  const pages: Array<MessageEmbed> = [];
+  const byMemberCount = (a: UserGroup, b: UserGroup) =>
+    b.members.length - a.members.length;
 
-  if (requestedGroupName === "" || group) {
-    const groupsPerPage = 4;
-    const pages: Array<MessageEmbed> = [];
-    const byMemberCount = (a: UserGroup, b: UserGroup) =>
-      b.members.length - a.members.length;
+  const copy = (
+    await groupRepository.find({
+      where: {
+        name: ILike(`%${requestedGroupName}%`),
+      },
+      relations: ["members"],
+    })
+  ).sort(byMemberCount);
 
-    const copy = (
-      await groupRepository.find({
-        where: {
-          name: ILike(`%${requestedGroupName}%`),
-        },
-        relations: ["members"],
-      })
-    ).sort(byMemberCount);
-
-    const pageAmount = Math.ceil(copy.length / groupsPerPage);
-
-    for (let i = 0; i < pageAmount; i++) {
-      const embed = new MessageEmbed({}).setAuthor(
-        "YesBot",
-        "https://cdn.discordapp.com/avatars/614101602046836776/61d02233797a400bc0e360098e3fe9cb.png?size=$%7BrequestedImageSize%7D"
-      );
-      embed.setDescription(
-        `Results for group "${requestedGroupName}" (Page ${
-          i + 1
-        } / ${pageAmount})`
-      );
-
-      const chunk = copy.splice(0, groupsPerPage);
-
-      chunk.forEach((group) => {
-        embed.addField("Group Name:", group.name, true);
-        embed.addField("Number of Members:", group.members.length, true);
-        embed.addField("Description", group.description || "-");
-        embed.addField("\u200B", "\u200B");
-      });
-
-      pages.push(embed);
-    }
-
-    const flip = async (
-      page: number,
-      shownPageMessage: Message,
-      reaction: MessageReaction
-    ) => {
-      if (page < 0) page = 0;
-      if (page >= pages.length) page = pages.length - 1;
-
-      await shownPageMessage.edit(message.author.toString(), {
-        embed: pages[page],
-      });
-      await reaction.users.remove(message.author.id);
-      setupPaging(page, shownPageMessage);
-    };
-
-    const setupPaging = async (currentPage: number, pagedMessage: Message) => {
-      const filter = (reaction: MessageReaction, user: User) => {
-        return (
-          ["⬅️", "➡️"].includes(reaction.emoji.name) &&
-          user.id === message.author.id
-        );
-      };
-
-      try {
-        const reactions = await pagedMessage.awaitReactions(filter, {
-          max: 1,
-          time: 60000,
-          errors: ["time"],
-        });
-        const first = reactions.first();
-        if (first.emoji.name === "⬅️") {
-          flip(currentPage - 1, pagedMessage, first);
-        }
-        if (first.emoji.name === "➡️") {
-          flip(currentPage + 1, pagedMessage, first);
-        }
-      } catch (error) {}
-    };
-
-    const sentMessagePromise = message.channel.send(pages[0]);
-    if (pages.length > 1) {
-      sentMessagePromise
-        .then(async (msg) => {
-          await msg.react("⬅️");
-          await msg.react("➡️");
-          return msg;
-        })
-        .then((msg) => setupPaging(0, msg));
-    }
-  } else if (group === undefined) {
-    message.reply("That group doesn't exist!");
+  if (copy.length === 0) {
+    await message.reply("No matching groups were found.")
     return;
+  }
+
+  const pageAmount = Math.ceil(copy.length / groupsPerPage);
+
+  for (let i = 0; i < pageAmount; i++) {
+    const embed = new MessageEmbed({}).setAuthor(
+      "YesBot",
+      "https://cdn.discordapp.com/avatars/614101602046836776/61d02233797a400bc0e360098e3fe9cb.png?size=$%7BrequestedImageSize%7D"
+    );
+    embed.setDescription(
+      `Results for group "${requestedGroupName}" (Page ${
+        i + 1
+      } / ${pageAmount})`
+    );
+
+    const chunk = copy.splice(0, groupsPerPage);
+
+    chunk.forEach((group) => {
+      embed.addField("Group Name:", group.name, true);
+      embed.addField("Number of Members:", group.members.length, true);
+      embed.addField("Description", group.description || "-");
+      embed.addField("\u200B", "\u200B");
+    });
+
+    pages.push(embed);
+  }
+
+  const flip = async (
+    page: number,
+    shownPageMessage: Message,
+    reaction: MessageReaction
+  ) => {
+    if (page < 0) page = 0;
+    if (page >= pages.length) page = pages.length - 1;
+
+    await shownPageMessage.edit(message.author.toString(), {
+      embed: pages[page],
+    });
+    await reaction.users.remove(message.author.id);
+    setupPaging(page, shownPageMessage);
+  };
+
+  const setupPaging = async (currentPage: number, pagedMessage: Message) => {
+    const filter = (reaction: MessageReaction, user: User) => {
+      return (
+        ["⬅️", "➡️"].includes(reaction.emoji.name) &&
+        user.id === message.author.id
+      );
+    };
+
+    try {
+      const reactions = await pagedMessage.awaitReactions(filter, {
+        max: 1,
+        time: 60000,
+        errors: ["time"],
+      });
+      const first = reactions.first();
+      if (first.emoji.name === "⬅️") {
+        flip(currentPage - 1, pagedMessage, first);
+      }
+      if (first.emoji.name === "➡️") {
+        flip(currentPage + 1, pagedMessage, first);
+      }
+    } catch (error) {}
+  };
+
+  const sentMessagePromise = message.channel.send(pages[0]);
+  if (pages.length > 1) {
+    sentMessagePromise
+      .then(async (msg) => {
+        await msg.react("⬅️");
+        await msg.react("➡️");
+        return msg;
+      })
+      .then((msg) => setupPaging(0, msg));
   }
 };
 
