08a3141f9a8ecc297ff8ef6300eb275b4756274c
Adrian Görisch <adrian.goerisch@newcubator.com>
2021-06-20 10:23:12 +0200

    fix bug with false mentions

--- src/programs/voice-on-demand.ts
+++ src/programs/voice-on-demand.ts
@@ -533,7 +533,7 @@ const requestOwnershipTransfer = async (
         .first()
     : channel.members.random().user;
   await botCommands.send(
-    `<@${claimingUser}>, is now the new owner of the room! You can now change the limit of it using \`!voice limit\`.`
+    `<@${claimingUser.id}>, is now the new owner of the room! You can now change the limit of it using \`!voice limit\`.`
   );
 
   await transferOwnership(currentMapping, claimingUser, channel);
