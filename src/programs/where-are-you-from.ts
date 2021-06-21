4587506f3d3e40128a377bc2bc74e0b016a0ad5f
huffle <63905701+huffle67@users.noreply.github.com>
2021-06-21 22:05:12 +0200

    update the bot welcome DM (#291)
    edit the bot welcome DM to include the !menu command

--- src/programs/where-are-you-from.ts
+++ src/programs/where-are-you-from.ts
@@ -60,7 +60,7 @@ const whereAreYouFrom = async (message: Message) => {
           welcomeMember(message.member.user, message.member.guild);
         }
         dmChannel.send(
-          `Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIf you'd like me to change your name on the server for you, just drop me a message and I will help you out! Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`
+          `Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIf you'd like me to change your name on the server for you, just message me `!menu` and I will help you out! Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`
         );
       });
 
