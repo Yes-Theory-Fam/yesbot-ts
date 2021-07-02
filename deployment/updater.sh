fc0375b66c2f8a1d6f584632d9614b579a2b57ea
Adrian Görisch <adrian.goerisch@newcubator.com>
2021-07-02 22:53:27 +0200

    update deployment pipeline

--- deployment/updater.sh
+++ deployment/updater.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-echo backing up bot
-
-# shellcheck disable=SC2046
-pg_dump -h localhost -p 5433 -U yesbot yesbot > db-backup/yesbot-$(date --rfc-3339=seconds | tr ' ' 'T').sql
+# shellcheck disable=SC2039
+if [[ -f databasebackup.sh ]];
+ then
+        echo backing up bot
+        ./databasebackup.sh
+fi
 
 echo restarting bot
 systemctl restart adrian-yesbot
+
+echo remove old backups
+# shellcheck disable=SC2010
+ls db-backup | grep yesbot | sort | head -n -4 | xargs rm
+
+echo update succesful
