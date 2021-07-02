c4f05526b511b95480ceb37155f387c92d8c77f4
Adrian Görisch <adrian.goerisch@newcubator.com>
2021-07-02 23:09:53 +0200

    update deployment pipeline

--- deployment/updater.sh
+++ deployment/updater.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
-# shellcheck disable=SC2039
+#!/bin/bash
+
+set -e
+
 if [[ -f databasebackup.sh ]];
  then
-        echo backing up bot
-        ./databasebackup.sh
+	echo backing up bot
+	./databasebackup.sh
 fi
+
+echo downloading new verison
+docker-compose -f /home/adrian/docker-compose.yml pull
 
 echo restarting bot
 systemctl restart adrian-yesbot
 
 echo remove old backups
-# shellcheck disable=SC2010
-ls db-backup | grep yesbot | sort | head -n -4 | xargs rm
+ls db-backup | grep yesbot | sort | head -n -4 | xargs rm || true
 
 echo update succesful
