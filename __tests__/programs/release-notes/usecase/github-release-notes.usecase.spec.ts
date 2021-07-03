02213cfe95bad8d443e0ac8afa525bb44d07dcfd
Adrian Görisch <adrian.goerisch@newcubator.com>
2021-07-04 00:45:07 +0200

    feat(*): send automatic release-notes on start
    the bot can now find the commit-messages since the last deployment and
    send them into the update channel

---
+++ __tests__/programs/release-notes/usecase/github-release-notes.usecase.spec.ts
@@ -0,0 +1,98 @@
+import { GithubReleaseNotesUsecase } from "../../../../src/programs/release-notes/usecase/github-release-notes.usecase";
+import {
+  GithubReleaseRepositoryType,
+  Node,
+} from "../../../../src/programs/release-notes/types/github-release.types";
+import { GithubReleaseNotesAccessor } from "../../../../src/programs/release-notes/acessor/github-release-notes.accessor";
+import { Release } from "@yes-theory-fam/database";
+
+jest.mock(
+  "../../../../src/programs/release-notes/acessor/github-release-notes.accessor"
+);
+
+describe("GithubReleaseNotesUsecase", () => {
+  let githubReleaseNotesAccessor: GithubReleaseNotesAccessor;
+  let githubReleaseNotesUsecase: GithubReleaseNotesUsecase;
+
+  beforeEach(() => {
+    const getCommitMessagesResponse = {
+      repository: {
+        object: {
+          history: {
+            nodes: [
+              {
+                messageHeadline:
+                  "chore(DOCKERFILE): docker image migrates th database by start",
+              },
+              {
+                messageHeadline:
+                  "chore(DOCKERFILE): docker image migrates th database by start",
+              },
+            ],
+          },
+        },
+      },
+    };
+    githubReleaseNotesUsecase = new GithubReleaseNotesUsecase();
+
+    githubReleaseNotesAccessor = new GithubReleaseNotesAccessor();
+    githubReleaseNotesAccessor.getCommitMessages = jest.fn(() =>
+      Promise.resolve(getCommitMessagesResponse)
+    );
+    githubReleaseNotesUsecase["githubReleaseNotesAccessor"] =
+      githubReleaseNotesAccessor;
+  });
+  it("should sort Releases", () => {
+    const input: GithubReleaseRepositoryType = {
+      repository: {
+        releases: {
+          edges: [
+            {
+              node: {
+                publishedAt: "2021-07-01T01:00:00Z",
+              } as Node,
+            },
+            {
+              node: {
+                publishedAt: "2020-07-01T01:00:00Z",
+              } as Node,
+            },
+          ],
+        },
+      },
+    };
+
+    const result = {
+      oldRelease: { node: { publishedAt: "2020-07-01T01:00:00Z" } },
+      newRelease: { node: { publishedAt: "2021-07-01T01:00:00Z" } },
+    };
+
+    expect(githubReleaseNotesUsecase["sortLatestReleases"](input)).toEqual(
+      result
+    );
+  });
+
+  it("should return git commit message", async () => {
+    const release: Release = {
+      id: "921332c4-64fb-46f2-8f8c-a6d6741934ed",
+      releaseName: "test",
+      releaseTime: "2020-07-01T01:00:00Z",
+      releaseTag: "1.0.0",
+      created: new Date("2020-07-01T01:00:00Z"),
+    };
+
+    githubReleaseNotesUsecase["getCommitMessages"](release, "2.0.0").then(
+      (result) => {
+        expect(result).toMatchSnapshot();
+      }
+    );
+  });
+
+  it("should create release notes", () => {
+    const releaseNotes = `> * chore(DOCKERFILE): docker image migrates th database by start
+> * chore(DOCKERFILE): docker image migrates th database by start\n`;
+    const result =
+      githubReleaseNotesUsecase["createReleaseNotes"](releaseNotes);
+    expect(result).toMatchSnapshot();
+  });
+});
---
+++ src/programs/release-notes/usecase/github-release-notes.usecase.ts
@@ -0,0 +1,111 @@
+import { GithubReleaseNotesAccessor } from "../acessor/github-release-notes.accessor";
+import prisma from "../../../prisma";
+import {
+  Edge,
+  GithubReleaseRepositoryType,
+} from "../types/github-release.types";
+import { Release } from "../../../../../database";
+
+interface ResponseType {
+  releaseNotes: string;
+  tagMessage: string;
+}
+
+export class GithubReleaseNotesUsecase {
+  private githubReleaseNotesAccessor;
+
+  constructor() {
+    this.githubReleaseNotesAccessor = new GithubReleaseNotesAccessor();
+  }
+
+  public async handle(): Promise<ResponseType> {
+    const { oldRelease, newRelease } =
+      await this.fetchLatestReleasesFormGitHub();
+    const latestRelease = await prisma.release.findFirst({
+      where: { releaseTime: newRelease.node.publishedAt },
+    });
+    let tagMessage = !!newRelease.node.description
+      ? newRelease.node.description
+      : "";
+    if (!!latestRelease) {
+      return {
+        tagMessage: `Hello everyone :wave: I'm back after a short break :sleeping:`,
+        releaseNotes: "",
+      };
+    }
+    const lastRelease = await this.findLastRelease(oldRelease);
+    const releaseNotes = await this.getCommitMessages(
+      lastRelease,
+      newRelease.node.tagName
+    );
+    await this.saveRelease(newRelease);
+
+    tagMessage = `**${newRelease.node.name}**\n${tagMessage}`;
+    return { tagMessage, releaseNotes: this.createReleaseNotes(releaseNotes) };
+  }
+
+  private async fetchLatestReleasesFormGitHub() {
+    const { oldRelease, newRelease } = await this.githubReleaseNotesAccessor
+      .getLastTwoTags()
+      .then((data) => {
+        return this.sortLatestReleases(data);
+      });
+    return { oldRelease, newRelease };
+  }
+
+  private sortLatestReleases(data: GithubReleaseRepositoryType) {
+    const edges = data.repository.releases.edges;
+    const sortedData = edges.sort((a, b) => {
+      return (
+        new Date(a.node.publishedAt).getTime() -
+        new Date(b.node.publishedAt).getTime()
+      );
+    });
+    const oldRelease = sortedData[0];
+    const newRelease = sortedData[1];
+    return { oldRelease, newRelease };
+  }
+
+  private async getCommitMessages(lastRelease: Release, tagName: string) {
+    const response = await this.githubReleaseNotesAccessor.getCommitMessages(
+      lastRelease.releaseTime,
+      tagName
+    );
+    console.log("response", JSON.stringify(response));
+    let releaseNotes: string = "";
+    response.repository.object.history.nodes.forEach((value) => {
+      releaseNotes += `> * ${value.messageHeadline}\n`;
+    });
+    return releaseNotes;
+  }
+
+  private async findLastRelease(oldRelease: Edge) {
+    let result = await prisma.release.findFirst({
+      where: { releaseTime: oldRelease.node.publishedAt },
+    });
+
+    if (!result) {
+      result = await this.saveRelease(oldRelease);
+    }
+    return result;
+  }
+
+  private async saveRelease(release: Edge) {
+    return await prisma.release.create({
+      data: {
+        releaseName: release.node.name,
+        releaseTime: release.node.publishedAt,
+        releaseTag: release.node.tagName,
+      },
+    });
+  }
+
+  private createReleaseNotes(releaseNotes: string) {
+    return `Hello everyone :wave: I'm back after a short break :sleeping:
+During this time I have been worked on a lot. Here is a small overview:
+
+${releaseNotes}
+So now back to work :tools:     
+`;
+  }
+}
