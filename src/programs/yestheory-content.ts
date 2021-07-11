556a1aa3212bc9b066e4f5127b17884def656651
Hasan Latch <hassoune80@gmail.com>
2021-07-11 22:21:34 +0200

    switched to check if user is bot

--- src/programs/yestheory-content.ts
+++ src/programs/yestheory-content.ts
@@ -1,25 +1,21 @@
 import Discord, { Message, TextChannel } from "discord.js";
 import { ChatNames } from "../collections/chat-names";
-import { hasRole } from "../common/moderator";
 
 //keep in mind this is very temporarily basic code, until we have the new events up and ready
 const YesTheoryUploadedPing = async (message: Message) => {
-  const user = message.author.id;
-  const MemberGuild = await message.guild.members.fetch(user);
-  if (hasRole(MemberGuild, "Support")) {
-    return;
+  if (message.author.bot) {
+    const channelDiscussion = message.guild.channels.cache.find(
+      (channel) => channel.name === ChatNames.YESTHEORY_DISCUSSION.toString()
+    ) as TextChannel;
+    const embed = new Discord.MessageEmbed()
+      .setColor("BLUE")
+      .setTitle("YesTheory Uploaded!")
+      .setDescription(
+        `Yes Theory posted a new video! Go check it out in ${message.channel.toString()} and talk about it here`
+      );
+    await channelDiscussion.send("@group YesTheoryUploads");
+    await channelDiscussion.send(embed);
   }
-  const channelDiscussion = message.guild.channels.cache.find(
-    (channel) => channel.name === ChatNames.YESTHEORY_DISCUSSION.toString()
-  ) as TextChannel;
-  const embed = new Discord.MessageEmbed()
-    .setColor("BLUE")
-    .setTitle("YesTheory Uploaded!")
-    .setDescription(
-      `Yes Theory posted a new video! Go check it out in ${message.channel.toString()} and talk about it here`
-    );
-  await channelDiscussion.send("@group YesTheoryUploads");
-  await channelDiscussion.send(embed);
 };
 
 export default YesTheoryUploadedPing;
