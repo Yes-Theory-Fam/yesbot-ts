ac754e4ecdf0cf81d08a39ed6831c7b4bb80d365
Hasan Latch <hassoune80@gmail.com>
2021-07-12 20:12:58 +0200

    fixed yestheoryuploaded ping (#361)
    * switched to check if user is bot
    
    * changed to check if message is from a webhook
    
    * added early return

--- src/programs/yestheory-content.ts
+++ src/programs/yestheory-content.ts
@@ -1,14 +1,12 @@
 import Discord, { Message, TextChannel } from "discord.js";
 import { ChatNames } from "../collections/chat-names";
-import { hasRole } from "../common/moderator";
 
 //keep in mind this is very temporarily basic code, until we have the new events up and ready
 const YesTheoryUploadedPing = async (message: Message) => {
-  const user = message.author.id;
-  const MemberGuild = await message.guild.members.fetch(user);
-  if (hasRole(MemberGuild, "Support")) {
+  if (!message.webhookID) {
     return;
   }
+
   const channelDiscussion = message.guild.channels.cache.find(
     (channel) => channel.name === ChatNames.YESTHEORY_DISCUSSION.toString()
   ) as TextChannel;
