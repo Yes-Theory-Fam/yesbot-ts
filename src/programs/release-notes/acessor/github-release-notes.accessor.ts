cd6c5e4f7e24db6a3b370d9260c3a49e53a1b871
Adrian Görisch <adrian.goerisch@newcubator.com>
2021-07-17 17:33:18 +0200

    fix(releasenotes): fix query (#367)

--- src/programs/release-notes/acessor/github-release-notes.accessor.ts
+++ src/programs/release-notes/acessor/github-release-notes.accessor.ts
@@ -17,6 +17,7 @@ export class GithubReleaseNotesAccessor {
                 name
                 publishedAt
                 tagName
+                description
               }
             }
           }
