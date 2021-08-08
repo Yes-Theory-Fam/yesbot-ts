09147e3fb38cc882088f4aadd8623ae374ebfdfb
Hasan Latch <hassoune80@gmail.com>
2021-08-09 00:49:33 +0200

    timeout evasion using the new event handlers

--- src/common/moderator.ts
+++ src/common/moderator.ts
@@ -5,6 +5,7 @@ import {
   PartialGuildMember,
   TextChannel,
 } from "discord.js";
+import prisma from "../prisma";
 
 export const isAuthorModerator = (message: Message): boolean => {
   if (message.member.roles.hoist) {
@@ -41,3 +42,36 @@ export const getMember = (userId: string): GuildMember => {
   const bot = require("..") as Client;
   return bot.guilds.resolve(process.env.GUILD_ID).members.resolve(userId);
 };
+
+export const isUserTimedOut = async (
+  member: GuildMember | PartialGuildMember
+): Promise<boolean> => {
+  return !!(await prisma.timedOutUsers.findFirst({
+    where: {
+      userId: member.id,
+    },
+  }));
+};
+
+export const wasUserTimedOut = async (
+  member: GuildMember | PartialGuildMember
+): Promise<boolean> => {
+  return !!(await prisma.timedOutUsers.findFirst({
+    where: {
+      userId: member.id,
+      TimedOutStatus: "ADDED",
+    },
+  }));
+};
+
+export const gainedRole = (
+  oldMember: GuildMember | PartialGuildMember,
+  newMember: GuildMember | PartialGuildMember,
+  roleName: string
+) => !hasRole(oldMember, roleName) && hasRole(newMember, roleName);
+
+export const lostRole = (
+  oldMember: GuildMember | PartialGuildMember,
+  newMember: GuildMember | PartialGuildMember,
+  roleName: string
+) => hasRole(oldMember, roleName) && !hasRole(newMember, roleName);
