bea7bee3ec7ce8b68d89aaf4ee9e71ab2f48f94d
Hasan Latch <hassoune80@gmail.com>
2021-08-09 21:53:25 +0200

    added loggers and split up the handler

--- src/programs/timeout-evasion.ts
+++ src/programs/timeout-evasion.ts
@@ -1,57 +1,84 @@
 import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
 import { GuildMember, PartialGuildMember } from "discord.js";
 import bot from "../index";
-import { isUserTimedOut, textLog } from "../common/moderator";
+import { textLog } from "../common/moderator";
 import Tools from "../common/tools";
 import prisma from "../prisma";
+import { createYesBotLogger } from "../log";
+
+const logger = createYesBotLogger("program", "timeout-evasion");
+
+const isUserTimedOut = async (
+  member: GuildMember | PartialGuildMember
+): Promise<boolean> => {
+  return !!(await prisma.timedOutUsers.findFirst({
+    where: {
+      userId: member.id,
+    },
+  }));
+};
 
 @Command({
   event: DiscordEvent.GUILD_MEMBER_UPDATE,
+  roleNamesAdded: ["Time Out"],
   description:
-    "This handle checks if the user was given or removed the Time Out role",
+    "This handler adds the user to the DB when he gains the time out role",
 })
-class TimeoutRoleStatus
+class TimeOutAdded implements CommandHandler<DiscordEvent.GUILD_MEMBER_UPDATE> {
+  async handle(member: GuildMember): Promise<void> {
+    if (await isUserTimedOut(member)) return;
+
+    const engineerRole = Tools.getRoleByName(
+      process.env.ENGINEER_ROLE_NAME,
+      member.guild
+    );
+    try {
+      await prisma.timedOutUsers.create({
+        data: {
+          userId: member.id,
+        },
+      });
+    } catch (e) {
+      logger.error("Failed to add user to the DB " + e);
+      await textLog(
+        `Failed to register <@${member.id}> to the DB, please contact a <@&${engineerRole.id}>`
+      );
+    }
+  }
+}
+
+@Command({
+  event: DiscordEvent.GUILD_MEMBER_UPDATE,
+  roleNamesRemoved: ["Time Out"],
+  description:
+    "This handler removes the user to the DB when he loses the time out role",
+})
+class TimeOutRemoved
   implements CommandHandler<DiscordEvent.GUILD_MEMBER_UPDATE>
 {
-  async handle(
-    oldMember: GuildMember | PartialGuildMember,
-    newMember: GuildMember | PartialGuildMember
-  ): Promise<void> {
-    if (await Tools.lostRole(oldMember, newMember, "Time Out")) {
-      try {
-        await prisma.timedOutUsers.delete({
-          where: {
-            userId: newMember.id,
-          },
-        });
-      } catch (e) {
-        await textLog(
-          `Failed to remove <@${newMember.id}> from timed out DB, please contact a @Developer`
-        );
-      }
-    }
-    if (
-      (await Tools.gainedRole(oldMember, newMember, "Time Out")) &&
-      !(await isUserTimedOut(newMember))
-    ) {
-      try {
-        await prisma.timedOutUsers.create({
-          data: {
-            userId: newMember.id,
-          },
-        });
-      } catch (e) {
-        await textLog(
-          `Failed to register <@${newMember.id}> to the DB, please contact a @Developer`
-        );
-      }
+  async handle(member: GuildMember): Promise<void> {
+    const engineerRole = Tools.getRoleByName(
+      process.env.ENGINEER_ROLE_NAME,
+      member.guild
+    );
+    try {
+      await prisma.timedOutUsers.delete({
+        where: {
+          userId: member.id,
+        },
+      });
+    } catch (e) {
+      logger.error("Failed to remove user from the DB " + e);
+      await textLog(
+        `Failed to remove <@${member.id}> from timed out DB, please contact a <@&${engineerRole.id}>`
+      );
     }
   }
 }
 
 @Command({
   event: DiscordEvent.MEMBER_LEAVE,
-  description: "This handle checks if the user left was timed out",
+  description: "This handler checks if the user left was timed out",
 })
 class ReportUserOnLeave implements CommandHandler<DiscordEvent.MEMBER_LEAVE> {
   async handle(member: GuildMember): Promise<void> {
@@ -62,7 +89,7 @@ class ReportUserOnLeave implements CommandHandler<DiscordEvent.MEMBER_LEAVE> {
 
 @Command({
   event: DiscordEvent.MEMBER_JOIN,
-  description: "This handle checks if the user joined was timed out",
+  description: "This handler checks if the user joined was timed out",
 })
 class ReportUserOnJoin implements CommandHandler<DiscordEvent.MEMBER_JOIN> {
   async handle(member: GuildMember): Promise<void> {
@@ -70,5 +97,8 @@ class ReportUserOnJoin implements CommandHandler<DiscordEvent.MEMBER_JOIN> {
     const guild = bot.guilds.resolve(process.env.GUILD_ID);
     const timeoutRole = Tools.getRoleByName("Time Out", guild);
     await member.roles.add(timeoutRole);
+    await textLog(
+      `<@${member.id}>, has rejoined and was assigned the timeout role for evading timeout`
+    );
   }
 }
