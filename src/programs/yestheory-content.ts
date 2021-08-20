92bff81a949f39a00991aa00b013b91a839e0597
Hasan Latch <hassoune80@gmail.com>
2021-08-20 19:18:51 +0200

    rewritten yestheory-content.ts

--- src/programs/yestheory-content.ts
+++ src/programs/yestheory-content.ts
@@ -1,23 +1,26 @@
 import Discord, { Message, TextChannel } from "discord.js";
 import { ChatNames } from "../collections/chat-names";
+import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
 
-//keep in mind this is very temporarily basic code, until we have the new events up and ready
-const YesTheoryUploadedPing = async (message: Message) => {
-  if (!message.webhookID) {
-    return;
+@Command({
+  event: DiscordEvent.MESSAGE,
+  channelNames: ["yestheoryposted"],
+  description: "Why is this a bunch of errors",
+})
+class YesTheoryUploadedPing implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message) {
+    if (!message.webhookID) return;
+
+    const channelDiscussion = message.guild.channels.cache.find(
+      (channel) => channel.name === ChatNames.YESTHEORY_DISCUSSION.toString()
+    ) as TextChannel;
+    const embed = new Discord.MessageEmbed()
+      .setColor("BLUE")
+      .setTitle("YesTheory Uploaded!")
+      .setDescription(
+        `Yes Theory posted a new video! Go check it out in ${message.channel.toString()} and talk about it here`
+      );
+    await channelDiscussion.send("@group YesTheoryUploads");
+    await channelDiscussion.send(embed);
   }
-
-  const channelDiscussion = message.guild.channels.cache.find(
-    (channel) => channel.name === ChatNames.YESTHEORY_DISCUSSION.toString()
-  ) as TextChannel;
-  const embed = new Discord.MessageEmbed()
-    .setColor("BLUE")
-    .setTitle("YesTheory Uploaded!")
-    .setDescription(
-      `Yes Theory posted a new video! Go check it out in ${message.channel.toString()} and talk about it here`
-    );
-  await channelDiscussion.send("@group YesTheoryUploads");
-  await channelDiscussion.send(embed);
-};
-
-export default YesTheoryUploadedPing;
+}
