82e62b53734725d4a627375114393590aeb10a75
Hasan Latch <hassoune80@gmail.com>
2021-08-21 13:52:07 +0200

    rewritten where-are-you-from.ts (#415)

--- src/programs/where-are-you-from.ts
+++ src/programs/where-are-you-from.ts
@@ -7,85 +7,11 @@ import {
   TextChannel,
   User,
 } from "discord.js";
+import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
 import { isRegistered, textLog } from "../common/moderator";
 import { CountryRoleFinder } from "../utils/country-role-finder";
 
 const regionCountries = ["USA"];
-
-const whereAreYouFrom = async (message: Message) => {
-  const newUser = !isRegistered(message.member);
-
-  if (newUser && !message.author.bot) {
-    const matchedCountries = CountryRoleFinder.getCountriesFromString(
-      message.content
-    );
-
-    if (matchedCountries.length === 0) {
-      await message.reply(
-        "You must send the flag or name of the country you are in!"
-      );
-      return;
-    }
-
-    if (matchedCountries.length > 1) {
-      await message.reply(
-        "Please only tell me 1 country for now, you can ask a member of the Support team about multiple nationalities :grin:"
-      );
-      return;
-    }
-
-    const countryToAssign = matchedCountries[0];
-    if (countryToAssign) {
-      const roleToAssign = CountryRoleFinder.getRoleForCountry(
-        countryToAssign,
-        message.guild
-      );
-      const memberDm = await message.author.createDM();
-
-      if (!roleToAssign) {
-        const moderatorRole = message.guild.roles.cache.find(
-          (r) => r.name === process.env.MODERATOR_ROLE_NAME
-        );
-        await textLog(
-          `${moderatorRole.toString()}: <@${
-            message.author.id
-          }> just requested role for country ${
-            countryToAssign.name
-          }, but I could not find it. Please make sure this role exists.`
-        );
-        await welcomeMember(message.author, message.guild);
-        return;
-      }
-      await message.member.roles.add(roleToAssign);
-      await message.react("👍");
-      const isCountryWithRegionRole = regionCountries.some(
-        (countryName) =>
-          CountryRoleFinder.getCountryByRole(roleToAssign.name) === countryName
-      );
-
-      const rules = message.guild.channels.cache.find(
-        (c) => c.name === "rules"
-      );
-      const generalInfo = message.guild.channels.cache.find(
-        (c) => c.name === "general-info"
-      );
-
-      if (!isCountryWithRegionRole) {
-        await welcomeMember(message.member.user, message.member.guild);
-      }
-      await memberDm.send(
-        `Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIf you'd like me to change your name on the server for you, just message me \`!menu\` and I will help you out! Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`
-      );
-
-      if (isCountryWithRegionRole) {
-        const lowerCaseCountry = CountryRoleFinder.getCountryByRole(
-          roleToAssign.name
-        ).toLowerCase();
-        await ghostPing(message, lowerCaseCountry);
-      }
-    }
-  }
-};
 
 const welcomeMember = async (user: User, guild: Guild) => {
   const memberRole = guild.roles.cache.find(({ name }) => name === "Member");
@@ -122,55 +48,133 @@ const ghostPing = async (message: Message, region: String) => {
   await ping.delete();
 };
 
-export const updateAfterRegionSelect = async (
-  oldMember: GuildMember | PartialGuildMember,
-  newMember: GuildMember | PartialGuildMember
+const findGeneralRole = (member: GuildMember | PartialGuildMember) =>
+  member.roles.cache.find(({ name }) => {
+    return regionCountries.some(
+      (countryName) => CountryRoleFinder.getCountryByRole(name) === countryName
+    );
+  });
+
+const hasSpecificRole = (
+  member: GuildMember | PartialGuildMember,
+  role: Role
 ) => {
-  const gainedRole = oldMember.roles.cache.size < newMember.roles.cache.size;
-  if (!gainedRole) return;
+  const emoji = role.name.split(" ").pop();
 
-  const findGeneralRole = (member: GuildMember | PartialGuildMember) =>
-    member.roles.cache.find(({ name }) => {
-      return regionCountries.some(
-        (countryName) =>
-          CountryRoleFinder.getCountryByRole(name) === countryName
+  return member.roles.cache.some(
+    ({ name }) => name.includes("(") && name.includes(emoji)
+  );
+};
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  channelNames: ["flag-drop"],
+  description:
+    "This handler is for when the user is selecting his country role",
+})
+class WhereAreYouFrom implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message): Promise<void> {
+    const newUser = !isRegistered(message.member);
+
+    if (newUser && !message.author.bot) {
+      const matchedCountries = CountryRoleFinder.getCountriesFromString(
+        message.content
       );
-    });
 
-  const hasSpecificRole = (
-    member: GuildMember | PartialGuildMember,
-    role: Role
-  ) => {
-    const emoji = role.name.split(" ").pop();
+      if (matchedCountries.length === 0) {
+        await message.reply(
+          "You must send the flag or name of the country you are in!"
+        );
+        return;
+      }
 
-    return member.roles.cache.some(
-      ({ name }) => name.includes("(") && name.includes(emoji)
-    );
-  };
+      if (matchedCountries.length > 1) {
+        await message.reply(
+          "Please only tell me 1 country for now, you can ask a member of the Support team about multiple nationalities :grin:"
+        );
+        return;
+      }
 
-  const generalRole = findGeneralRole(oldMember);
-  if (generalRole && hasSpecificRole(newMember, generalRole)) {
-    await newMember.roles.remove(generalRole);
-    const hasNoOtherCountry = oldMember.roles.cache.every(
-      (role) => role.id !== process.env.MEMBER_ROLE_ID
-    );
+      const countryToAssign = matchedCountries[0];
+      if (countryToAssign) {
+        const roleToAssign = CountryRoleFinder.getRoleForCountry(
+          countryToAssign,
+          message.guild
+        );
+        const memberDm = await message.author.createDM();
 
-    if (hasNoOtherCountry) {
-      await welcomeMember(oldMember.user, oldMember.guild);
+        if (!roleToAssign) {
+          const moderatorRole = message.guild.roles.cache.find(
+            (r) => r.name === process.env.MODERATOR_ROLE_NAME
+          );
+          await textLog(
+            `${moderatorRole.toString()}: <@${
+              message.author.id
+            }> just requested role for country ${
+              countryToAssign.name
+            }, but I could not find it. Please make sure this role exists.`
+          );
+          await welcomeMember(message.author, message.guild);
+          return;
+        }
+        await message.member.roles.add(roleToAssign);
+        await message.react("👍");
+        const isCountryWithRegionRole = regionCountries.some(
+          (countryName) =>
+            CountryRoleFinder.getCountryByRole(roleToAssign.name) ===
+            countryName
+        );
+
+        const rules = message.guild.channels.cache.find(
+          (c) => c.name === "rules"
+        );
+        const generalInfo = message.guild.channels.cache.find(
+          (c) => c.name === "general-info"
+        );
+
+        if (!isCountryWithRegionRole) {
+          await welcomeMember(message.member.user, message.member.guild);
+        }
+        await memberDm.send(
+          `Hey! My name is YesBot, I'm so happy to see you've made it into our world, we really hope you stick around!\n\nIn the meantime, you should checkout ${rules.toString()} and ${generalInfo.toString()} , they contain a lot of good-to-knows about our server and what cool stuff you can do.\nIf you'd like me to change your name on the server for you, just message me \`!menu\` and I will help you out! Then I can introduce you to our family :grin:\n\nI know Discord can be a lot to take in at first, trust me, but it's really quite a wonderful place.`
+        );
+
+        if (isCountryWithRegionRole) {
+          const lowerCaseCountry = CountryRoleFinder.getCountryByRole(
+            roleToAssign.name
+          ).toLowerCase();
+          await ghostPing(message, lowerCaseCountry);
+        }
+      }
     }
   }
-};
+}
 
-const multipleCountries = async (message: string): Promise<boolean> => {
-  const seperatedEmojis = message.match(/\p{Emoji_Presentation}/gu);
-  if (
-    seperatedEmojis[0] + seperatedEmojis[1] !==
-      seperatedEmojis[2] + seperatedEmojis[3] &&
-    seperatedEmojis.length > 2
-  ) {
-    return true;
+@Command({
+  event: DiscordEvent.GUILD_MEMBER_UPDATE,
+  description:
+    "This handler checks if the user has selected his region after dropping his flag",
+})
+class UpdateAfterRegionSelect
+  implements CommandHandler<DiscordEvent.GUILD_MEMBER_UPDATE>
+{
+  async handle(
+    oldMember: GuildMember | PartialGuildMember,
+    newMember: GuildMember | PartialGuildMember
+  ): Promise<void> {
+    const gainedRole = oldMember.roles.cache.size < newMember.roles.cache.size;
+    if (!gainedRole) return;
+
+    const generalRole = findGeneralRole(oldMember);
+    if (generalRole && hasSpecificRole(newMember, generalRole)) {
+      await newMember.roles.remove(generalRole);
+      const hasNoOtherCountry = oldMember.roles.cache.every(
+        (role) => role.id !== process.env.MEMBER_ROLE_ID
+      );
+
+      if (hasNoOtherCountry) {
+        await welcomeMember(oldMember.user, oldMember.guild);
+      }
+    }
   }
-  return false;
-};
-
-export default whereAreYouFrom;
+}
