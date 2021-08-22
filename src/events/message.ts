8a1bf5bda123fbca511c64c7e06d5a400e123837
Hasan Latch <hassoune80@gmail.com>
2021-08-22 15:47:01 +0200

    Chore: refactor dm-menu.ts (#441)
    * much better
    
    * avoiding a nuke
    
    * ran lint:fix

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,7 +1,6 @@
 import { DMChannel, GuildChannel, Message, TextChannel } from "discord.js";
 import {
   BirthdayManager,
-  DMMenu,
   ExportManager,
   Game,
   GroupManager,
@@ -13,8 +12,7 @@ import {
   TopicManager,
   VoiceOnDemand,
 } from "../programs";
-import state from "../common/state";
-import { getMember, hasRole, textLog } from "../common/moderator";
+import { hasRole, textLog } from "../common/moderator";
 import {
   abuseMe,
   addVote,
@@ -28,12 +26,11 @@ import {
   postDailyMessage,
   saveToDb,
 } from "../programs/daily-challenge";
-import { ChatNames } from "../collections/chat-names";
 import timeoutUser from "../programs/timeout";
 
 const message = async (msg: Message) => {
   if (msg.channel.type === "dm" && !msg.author.bot) {
-    await routeDm(msg);
+    return;
   } else {
     await routeMessage(msg);
   }
@@ -196,31 +193,6 @@ const routeMessage = async (message: Message) => {
     await GroupManager(message, true);
 
   if (words.includes("@group")) await GroupManager(message, false);
-};
-
-const routeDm = async (message: Message) => {
-  const member = getMember(message.author.id);
-  const dmChannel = message.channel;
-
-  if (!member) {
-    await dmChannel.send(
-      "Hey, I am the bot of the Yes Theory Fam Discord Server :) Looks like you are not on it currently, so I cannot really do a lot for you. If you'd like to join, click here: https://discord.gg/yestheory"
-    );
-    return;
-  }
-
-  if (state.ignoredGroupDMs.includes(dmChannel.id)) return;
-
-  const command = message.content.split(" ")[0];
-  switch (command) {
-    case "!menu":
-      await DMMenu.showMenu(message);
-      break;
-    // When nothing else makes sense we just guess that they are playing a game.
-    // handleGameInput will drop the message if it doesn't understand either.
-    default:
-      Game.handleGameInput(message);
-  }
 };
 
 export default message;
--- src/programs/dm-menu.ts
+++ src/programs/dm-menu.ts
@@ -7,6 +7,14 @@ import {
 } from "discord.js";
 import state from "../common/state";
 import { proposeNameChange } from "../common/custom-methods";
+import {
+  Command,
+  CommandHandler,
+  DiscordEvent,
+  EventLocation,
+} from "../event-distribution";
+import { getMember } from "../common/moderator";
+import { Game } from ".";
 
 const removeIgnore = (channel: DMChannel) => {
   const index = state.ignoredGroupDMs.indexOf(channel.id);
@@ -15,48 +23,77 @@ const removeIgnore = (channel: DMChannel) => {
   }
 };
 
-// This will probably need some larger refactoring when more functionality is added.
-export const showMenu = async (message: Message) => {
-  const dmChannel = message.channel as DMChannel;
+@Command({
+  event: DiscordEvent.MESSAGE,
+  location: EventLocation.DIRECT_MESSAGE,
+  description: "This handler is temporarily until we refactor games.ts",
+})
+class HandleGameInput implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message) {
+    //Since game.ts isn't refactored this is a temporarily solution for it not to be "nuked"
+    Game.handleGameInput(message);
+  }
+}
 
-  const nameChangeMessage = await message.reply(
-    "Hey, I'm just a bot! Most of what I can do, I do on the YesFam discord, so talk to me there instead! I can help you change your name, though, if you're new around here. Click the :baby: if you want to change your name!"
-  );
-  await nameChangeMessage.react("👶");
-  const filter: CollectorFilter = (reaction: MessageReaction, user: User) =>
-    reaction.emoji.name === "👶" && !user.bot;
-  try {
-    const reactions = await nameChangeMessage.awaitReactions(filter, {
-      time: 60000,
-      max: 1,
-    });
-    if (reactions.size === 0) throw "No reactions";
+@Command({
+  event: DiscordEvent.MESSAGE,
+  trigger: "!menu",
+  location: EventLocation.DIRECT_MESSAGE,
+  description: "This handler is for the DM menu",
+})
+class ShowMenu implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message): Promise<void> {
+    const member = getMember(message.author.id);
+    const dmChannel = message.channel as DMChannel;
 
-    const requestMessage = await dmChannel.send(
-      "Okay, what's your name then? Please only respond with your name like Henry or Julie, that makes things easier for the Supports! :upside_down:"
-    );
-    state.ignoredGroupDMs.push(dmChannel.id);
-    const nameMessage = await dmChannel.awaitMessages(
-      (_, user: User) => !user.bot,
-      { time: 60000, max: 1 }
-    );
-    removeIgnore(dmChannel);
-
-    if (nameMessage.size === 0) {
-      requestMessage.delete();
-      throw "No response";
+    if (!member) {
+      await dmChannel.send(
+        "Hey, I am the bot of the Yes Theory Fam Discord Server :) Looks like you are not on it currently, so I cannot really do a lot for you. If you'd like to join, click here: https://discord.gg/yestheory"
+      );
+      return;
     }
 
-    const requestedName = nameMessage.first().content;
-    proposeNameChange(requestedName, message);
-    await requestMessage.delete();
-  } catch (err) {
-    removeIgnore(dmChannel);
-    // Time's up; nothing to do here, really
-    dmChannel.send(
-      "Because of technical reasons I can only wait 60 seconds for a reaction. I removed the other message to not confuse you. If you need anything from me, just drop me a message!"
-    );
-  }
+    if (state.ignoredGroupDMs.includes(dmChannel.id)) return;
 
-  await nameChangeMessage.delete();
-};
+    const nameChangeMessage = await message.reply(
+      "Hey, I'm just a bot! Most of what I can do, I do on the YesFam discord, so talk to me there instead! I can help you change your name, though, if you're new around here. Click the :baby: if you want to change your name!"
+    );
+    await nameChangeMessage.react("👶");
+    const filter: CollectorFilter = (reaction: MessageReaction, user: User) =>
+      reaction.emoji.name === "👶" && !user.bot;
+    try {
+      const reactions = await nameChangeMessage.awaitReactions(filter, {
+        time: 60000,
+        max: 1,
+      });
+      if (reactions.size === 0) throw "No reactions";
+
+      const requestMessage = await dmChannel.send(
+        "Okay, what's your name then? Please only respond with your name like Henry or Julie, that makes things easier for the Supports! :upside_down:"
+      );
+      state.ignoredGroupDMs.push(dmChannel.id);
+      const nameMessage = await dmChannel.awaitMessages(
+        (_, user: User) => !user.bot,
+        { time: 60000, max: 1 }
+      );
+      removeIgnore(dmChannel);
+
+      if (nameMessage.size === 0) {
+        requestMessage.delete();
+        throw "No response";
+      }
+
+      const requestedName = nameMessage.first().content;
+      proposeNameChange(requestedName, message);
+      await requestMessage.delete();
+    } catch (err) {
+      removeIgnore(dmChannel);
+      // Time's up; nothing to do here, really
+      dmChannel.send(
+        "Because of technical reasons I can only wait 60 seconds for a reaction. I removed the other message to not confuse you. If you need anything from me, just drop me a message!"
+      );
+    }
+
+    await nameChangeMessage.delete();
+  }
+}
