d91866001b50df2556a8b88d2b838caea052ac68
Hasan Latch <hassoune80@gmail.com>
2021-08-22 15:49:16 +0200

    Chore: refactore separator.ts (#444)
    * refactored separator.ts
    
    * i swear i ran lint:fix

--- src/programs/separators.ts
+++ src/programs/separators.ts
@@ -1,56 +1,73 @@
 import { GuildMember, PartialGuildMember } from "discord.js";
+import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
 
 const separatorStart = "\u2063";
 
-export const separatorOnRoleAdd = (
-  oldMember: GuildMember | PartialGuildMember,
-  newMember: GuildMember | PartialGuildMember
-) => {
-  // No role added
-  if (oldMember.roles.cache.size >= newMember.roles.cache.size) return;
-  const addedRole = newMember.roles.cache
-    .filter(
-      (role) => !oldMember.roles.cache.some((oldRole) => oldRole.id === role.id)
-    )
-    .first();
+@Command({
+  event: DiscordEvent.GUILD_MEMBER_UPDATE,
+  description: "This handler is to add the separator role if necessary",
+})
+class SeparatorOnRoleAdd
+  implements CommandHandler<DiscordEvent.GUILD_MEMBER_UPDATE>
+{
+  async handle(
+    oldMember: GuildMember | PartialGuildMember,
+    newMember: GuildMember | PartialGuildMember
+  ) {
+    if (oldMember.roles.cache.size >= newMember.roles.cache.size) return;
+    const addedRole = newMember.roles.cache
+      .filter(
+        (role) =>
+          !oldMember.roles.cache.some((oldRole) => oldRole.id === role.id)
+      )
+      .first();
 
-  if (addedRole.name.startsWith(separatorStart)) return;
-  const { guild } = oldMember;
+    if (addedRole.name.startsWith(separatorStart)) return;
+    const { guild } = oldMember;
 
-  const separators = guild.roles.cache
-    .filter(({ name }) => name.startsWith(separatorStart))
-    .array();
+    const separators = guild.roles.cache
+      .filter(({ name }) => name.startsWith(separatorStart))
+      .array();
 
-  // We can find the matching separator by sorting the array of the separators and the added role.
-  //   The separator above the added role is the correct one. If there is none, no separator is added.
-  const sorted = [...separators, addedRole].sort((a, b) =>
-    b.comparePositionTo(a)
-  );
-  const addedIndex = sorted.indexOf(addedRole);
-  const separatorIndex = addedIndex - 1;
+    // We can find the matching separator by sorting the array of the separators and the added role.
+    //   The separator above the added role is the correct one. If there is none, no separator is added.
+    const sorted = [...separators, addedRole].sort((a, b) =>
+      b.comparePositionTo(a)
+    );
+    const addedIndex = sorted.indexOf(addedRole);
+    const separatorIndex = addedIndex - 1;
 
-  if (separatorIndex < 0) return;
+    if (separatorIndex < 0) return;
 
-  newMember.roles.add(sorted[separatorIndex]);
-};
+    newMember.roles.add(sorted[separatorIndex]);
+  }
+}
 
-export const separatorOnRoleRemove = (
-  oldMember: GuildMember | PartialGuildMember,
-  newMember: GuildMember | PartialGuildMember
-) => {
-  if (oldMember.roles.cache.size <= newMember.roles.cache.size) return;
+@Command({
+  event: DiscordEvent.GUILD_MEMBER_UPDATE,
+  description: "This handler is to remove the separator role if necessary",
+})
+class SeparatorOnRoleRemove
+  implements CommandHandler<DiscordEvent.GUILD_MEMBER_UPDATE>
+{
+  async handle(
+    oldMember: GuildMember | PartialGuildMember,
+    newMember: GuildMember | PartialGuildMember
+  ) {
+    if (oldMember.roles.cache.size <= newMember.roles.cache.size) return;
 
-  const memberRolesSorted = newMember.roles.cache
-    .array()
-    .sort((a, b) => b.comparePositionTo(a));
+    const memberRolesSorted = newMember.roles.cache
+      .array()
+      .sort((a, b) => b.comparePositionTo(a));
 
-  // A separator role can be removed if there is no role or another separator role below it.
-  const separatorsToRemove = memberRolesSorted.filter(
-    (role, index) =>
-      role.name.startsWith(separatorStart) &&
-      (index === memberRolesSorted.length - 2 ||
-        memberRolesSorted[index + 1]?.name.startsWith(separatorStart))
-  );
+    // A separator role can be removed if there is no role or another separator role below it.
+    const separatorsToRemove = memberRolesSorted.filter(
+      (role, index) =>
+        role.name.startsWith(separatorStart) &&
+        (index === memberRolesSorted.length - 2 ||
+          memberRolesSorted[index + 1]?.name.startsWith(separatorStart))
+    );
 
-  separatorsToRemove.forEach((role) => newMember.roles.remove(role));
-};
+    separatorsToRemove.forEach((role) => newMember.roles.remove(role));
+  }
+}
