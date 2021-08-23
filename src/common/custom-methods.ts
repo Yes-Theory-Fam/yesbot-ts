3bd217cc3a508158c269857924dc504b1cd59499
Hasan Latch <hassoune80@gmail.com>
2021-08-23 18:33:02 +0200

    chore: refactored custom-methods.ts

--- src/common/custom-methods.ts
+++ src/common/custom-methods.ts
@@ -1,12 +1,196 @@
 import { DMChannel, Message, User } from "discord.js";
-import { getMember, textLog } from "./moderator";
+import { getMember, hasRole, textLog } from "./moderator";
 import Tools from "./tools";
 import { createYesBotLogger } from "../log";
 import axios from "axios";
+import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
 
 const logger = createYesBotLogger("common", "CustomMethods");
 
-export const sendLove = (message: Message) => {
+@Command({
+  event: DiscordEvent.MESSAGE,
+  description: "This handler is to filter certain messages on the server.",
+})
+class Filter implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message): Promise<void> {
+    const messageContent = message.content;
+    const mentionedMembers = message.mentions.users.size;
+    const member = message.member;
+
+    if (mentionedMembers > 20 && !message.author.bot) {
+      if (
+        !(
+          hasRole(member, "Support") ||
+          hasRole(member, "Yes Theory") ||
+          hasRole(member, "Seek Discomfort")
+        )
+      ) {
+        member.createDM().then((dm) => {
+          dm.send(
+            "Hey there! You tagged more than 20 people in a single message. The message has been deleted and you have beeen timed out. Here is the message sent: "
+          );
+          dm.send(messageContent);
+        });
+        await message.delete();
+        const supportRole = Tools.getRoleByName(
+          process.env.MODERATOR_ROLE_NAME,
+          message.guild
+        );
+        const timeoutRole = Tools.getRoleByName("Time Out", message.guild);
+        await message.member.roles.add(timeoutRole);
+        textLog(
+          `<@&${supportRole.id}>: <@${message.author.id}> just tagged more than 20 people in a single message in <#${message.channel.id}>. The message has been deleted and they have beeen timed out.`
+        ).then(() => textLog(`Message content was: ${messageContent}`));
+      }
+      return;
+    }
+
+    if (messageContent.match(/(nigga|nigger)/i)) {
+      await message.delete();
+      member
+        .createDM()
+        .then((dm) =>
+          dm.send(
+            `Usage of the N word is absolutely banned within this server. Please refer to the <#450102410262609943>.`
+          )
+        );
+      return;
+    }
+  }
+}
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  description: "This handler is for custom messages that do not have a specific trigger.",
+})
+class CustomMessageMethods implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message): Promise<void> {
+    const messageContent = message.content;
+
+    if (messageContent.match(/^(yesbot).*(\?)$/gi)) randomReply(message);
+
+    if (
+      messageContent.match(
+        /yesbot i love you|yesbot i love u|i love you yesbot/i
+      )
+    ) sendLove(message);
+
+    if (messageContent.match(/^(F)$/i)) await message.react("🇫")
+
+    if (messageContent.match(/(abooz|mod abuse)/i)) await message.react("👀");
+  }
+}
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  trigger: "!translate",
+  channelNames: ["chat", "chat-too", "4th-chat", "chat-v"],
+  description: "This handler is for the translate command.",
+})
+class AbuseMeCommand implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message): Promise<void> {
+    const taggedUser = message.mentions.users?.first();
+    let replies = [
+      'You are as useless as the "ueue" in "Queue".',
+      "As an outsider, what do you think of the human race?",
+      "Brains aren't everything. In your case they're nothing.",
+      "Do you think before you speak or is thinking a rare event in your life?",
+      "I don't engage in mental combat with the unarmed.",
+      "I don't think you are stupid. You just have a bad luck when thinking.",
+      "I would love to insult you... but that would be beyond the level of your intelligence.",
+      "I'd agree with you but then we'd both be wrong",
+      "I'd like to see things from your point of view but I can't seem to get my head that far up my butt.",
+      "I'm jealous of all the people that haven't met you!",
+      "If I had a dollar for everytime you said something smart, I'd be broke.",
+      "Is your a$$ jealous of the amount of sh!t that just came out of your mouth?",
+      "It's better to let someone think you are an Idiot than to open your mouth and prove it.",
+      "Maybe you need to be on timeout",
+      "Ordinarily people live and learn. You just live.",
+      "Stupidity is not a crime so you are free to go.",
+      "Support bacteria - they're the only culture some people have.",
+      "The last thing I want to do is hurt you. But it's still on the list.",
+      "Two wrongs don't make a right, take your parents as an example.",
+      "Yesbot does not love you specifically",
+      "You are a bitch",
+      "You sound reasonable. It must be time to up my medication!",
+    ];
+
+    const userToTag = taggedUser ? taggedUser.id : message.author.id;
+    const cleanMessage = message.cleanContent.split(/\s+/);
+    cleanMessage.shift();
+    const joinedMsg = cleanMessage.join(" ");
+    const reply = `<@${userToTag}> *\`${joinedMsg}\`* translated to English means *${
+      replies[Math.floor(Math.random() * replies.length)]
+    }*`;
+    message.channel.send(reply);
+  }
+}
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  trigger: "!delete",
+  allowedRoles: ["Support"],
+  description: "This handler is to delete message(s) in the requested text channel.",
+})
+class DeleteMessages implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(botMessage: Message): Promise<void> {
+    try {
+      const words = Tools.stringToWords(botMessage.content);
+      words.shift();
+      const messagesToDelete = Number(words[0]);
+      if (
+        !isNaN(messagesToDelete) &&
+        !(botMessage.channel instanceof DMChannel)
+      ) {
+        await botMessage.channel.bulkDelete(messagesToDelete);
+      }
+    } catch (err) {
+      logger.error("Error deleting messages: ", err);
+    }
+  }
+}
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  trigger: "!addvote",
+  description: "This handler is to add to any requested changes the two reactions thumbs reaction to convert to a poll",
+})
+class AddVote implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(botMessage: Message): Promise<void> {
+    try {
+      const words = Tools.stringToWords(botMessage.content);
+      words.shift();
+      const messageId = words[0];
+      const messageToVote = await botMessage.channel.messages.resolve(
+        messageId
+      );
+      if (!messageToVote) botMessage.react("👎");
+      else
+        botMessage
+          .delete()
+          .then(() => messageToVote.react("👍"))
+          .then(() => messageToVote.react("👎"));
+    } catch (err) {
+      logger.error("Error adding voting: ", err);
+    }
+  }
+}
+
+const randomReply = (message: Message) => {
+  let replies = [
+    "yes.",
+    "probably.",
+    "doubtful.",
+    "i'm afraid I don't know that one",
+    "absolutely not.",
+    "not a chance.",
+    "definitely.",
+    "very very very unlikely",
+  ];
+  message.reply(`${replies[Math.floor(Math.random() * replies.length)]}`);
+};
+
+const sendLove = (message: Message) => {
   const loveArr = [
     "I love you too, Cutiepie",
     "I will find you and I will love you.",
@@ -26,55 +210,17 @@ export const sendLove = (message: Message) => {
   message.react("😍");
 };
 
-export const randomReply = (message: Message) => {
-  let replies = [
-    "yes.",
-    "probably.",
-    "doubtful.",
-    "i'm afraid I don't know that one",
-    "absolutely not.",
-    "not a chance.",
-    "definitely.",
-    "very very very unlikely",
-  ];
-  message.reply(`${replies[Math.floor(Math.random() * replies.length)]}`);
-};
+export const getFirstColumnFromGoogleSheet = async (
+  sheetId: string
+): Promise<string[]> => {
+  const apiKey = process.env.GOOGLE_API_KEY;
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
 
-export const abuseMe = (message: Message) => {
-  const taggedUser = message.mentions.users?.first();
-  let replies = [
-    'You are as useless as the "ueue" in "Queue".',
-    "As an outsider, what do you think of the human race?",
-    "Brains aren't everything. In your case they're nothing.",
-    "Do you think before you speak or is thinking a rare event in your life?",
-    "I don't engage in mental combat with the unarmed.",
-    "I don't think you are stupid. You just have a bad luck when thinking.",
-    "I would love to insult you... but that would be beyond the level of your intelligence.",
-    "I'd agree with you but then we'd both be wrong",
-    "I'd like to see things from your point of view but I can't seem to get my head that far up my butt.",
-    "I'm jealous of all the people that haven't met you!",
-    "If I had a dollar for everytime you said something smart, I'd be broke.",
-    "Is your a$$ jealous of the amount of sh!t that just came out of your mouth?",
-    "It's better to let someone think you are an Idiot than to open your mouth and prove it.",
-    "Maybe you need to be on timeout",
-    "Ordinarily people live and learn. You just live.",
-    "Stupidity is not a crime so you are free to go.",
-    "Support bacteria - they're the only culture some people have.",
-    "The last thing I want to do is hurt you. But it's still on the list.",
-    "Two wrongs don't make a right, take your parents as an example.",
-    "Yesbot does not love you specifically",
-    "You are a bitch",
-    "You sound reasonable. It must be time to up my medication!",
-  ];
+  const response = await axios.get(url);
+  const data = await response.data;
+  const rows = data.values;
 
-  const userToTag = taggedUser ? taggedUser.id : message.author.id;
-  const cleanMessage = message.cleanContent.split(/\s+/);
-  cleanMessage.shift();
-  const joinedMsg = cleanMessage.join(" ");
-  const reply = `<@${userToTag}> *\`${joinedMsg}\`* translated to English means *${
-    replies[Math.floor(Math.random() * replies.length)]
-  }*`;
-  message.channel.send(reply);
+  return rows.flatMap((row: string[]) => row);
 };
 
 export const proposeNameChange = async (name: string, botMessage: Message) => {
@@ -115,50 +261,4 @@ export const proposeNameChange = async (name: string, botMessage: Message) => {
   } catch (err) {
     logger.error("(proposeNameChange) Error changing name: ", err);
   }
-};
-
-export const deleteMessages = async (botMessage: Message) => {
-  try {
-    const words = Tools.stringToWords(botMessage.content);
-    words.shift();
-    const messagesToDelete = Number(words[0]);
-    if (
-      !isNaN(messagesToDelete) &&
-      !(botMessage.channel instanceof DMChannel)
-    ) {
-      await botMessage.channel.bulkDelete(messagesToDelete);
-    }
-  } catch (err) {
-    logger.error("Error deleting messages: ", err);
-  }
-};
-
-export const addVote = async (botMessage: Message) => {
-  try {
-    const words = Tools.stringToWords(botMessage.content);
-    words.shift();
-    const messageId = words[0];
-    const messageToVote = await botMessage.channel.messages.resolve(messageId);
-    if (!messageToVote) botMessage.react("👎");
-    else
-      botMessage
-        .delete()
-        .then(() => messageToVote.react("👍"))
-        .then(() => messageToVote.react("👎"));
-  } catch (err) {
-    logger.error("Error adding voting: ", err);
-  }
-};
-
-export const getFirstColumnFromGoogleSheet = async (
-  sheetId: string
-): Promise<string[]> => {
-  const apiKey = process.env.GOOGLE_API_KEY;
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
-
-  const response = await axios.get(url);
-  const data = await response.data;
-  const rows = data.values;
-
-  return rows.flatMap((row: string[]) => row);
 };
--- src/events/message.ts
+++ src/events/message.ts
@@ -1,4 +1,4 @@
-import { DMChannel, GuildChannel, Message, TextChannel } from "discord.js";
+import { GuildChannel, Message, TextChannel } from "discord.js";
 import {
   BirthdayManager,
   ExportManager,
@@ -10,15 +10,6 @@ import {
   TopicManager,
   VoiceOnDemand,
 } from "../programs";
-import { hasRole, textLog } from "../common/moderator";
-import {
-  abuseMe,
-  addVote,
-  deleteMessages,
-  randomReply,
-  sendLove,
-} from "../common/custom-methods";
-import Tools from "../common/tools";
 import {
   dailyChallenge,
   postDailyMessage,
@@ -34,47 +25,6 @@ const message = async (msg: Message) => {
 };
 
 const routeMessage = async (message: Message) => {
-  const mentionedMembers = message.mentions.users.size;
-  if (mentionedMembers > 20 && !message.author.bot) {
-    const whitelistedRoles = ["support", "yes theory", "seek discomfort"];
-    const hasWhitelistedRole = message.member.roles.cache.some((r) =>
-      whitelistedRoles.includes(r.name.toLowerCase())
-    );
-    if (hasWhitelistedRole) return;
-
-    message.author.createDM().then((dm: DMChannel) => {
-      dm.send(
-        "Hey there! You tagged more than 20 people in a single message. The message has been deleted and you have beeen timed out. Here is the message sent: "
-      );
-      dm.send(message.content);
-    });
-    await message.delete();
-    const timeoutRole = Tools.getRoleByName("Time Out", message.guild);
-    const supportRole = Tools.getRoleByName(
-      process.env.MODERATOR_ROLE_NAME,
-      message.guild
-    );
-    await message.member.roles.add(timeoutRole);
-    textLog(
-      `<@&${supportRole.id}>: <@${message.author.id}> just tagged more than 20 people in a single message in <#${message.channel.id}>. The message has been deleted and they have beeen timed out.`
-    ).then(() => textLog(`Message content was: ${message.content}`));
-  }
-
-  const filteredWords = ["nigger", "nigga"];
-  const lowerCaseMessage = message.content.toLowerCase();
-  if (filteredWords.some((word) => lowerCaseMessage.includes(word))) {
-    await message.delete();
-    message.author
-      .createDM()
-      .then((dm) =>
-        dm.send(
-          `Usage of the N word is absolutely banned within this server. Please refer to the <#450102410262609943>.`
-        )
-      );
-
-    return;
-  }
-
   const words = message.content.split(/\s+/);
   const channel = <TextChannel>message.channel;
   const firstWord = words[0];
@@ -85,13 +35,6 @@ const routeMessage = async (message: Message) => {
       if (firstWord === "!video") {
         await message.reply("https://youtu.be/v-JOe-xqPN0");
       }
-      break;
-
-    case "chat":
-    case "chat-too":
-    case "4th-chat":
-    case "chat-v":
-      if (firstWord === "!translate") abuseMe(message);
       break;
 
     case "trends":
@@ -156,28 +99,7 @@ const routeMessage = async (message: Message) => {
   if (firstWord === "!topic") await TopicManager.topics(message);
   // if (firstWord === "!fiyesta") Ticket(message, "fiyesta");
   if (firstWord === "!shoutout") await Ticket(message, "shoutout");
-  if (firstWord === "!addvote") await addVote(message);
-  if (firstWord === "!delete")
-    hasRole(message.member, "Support") ? await deleteMessages(message) : null;
   if (firstWord === "!role") await ReactRole(message);
-  if (firstWord === "F") await message.react("🇫");
-  if (
-    ["i love u yesbot", "i love you yesbot", "yesbot i love you "].includes(
-      message.content.toLowerCase()
-    )
-  )
-    sendLove(message);
-  if (
-    message.content.toLowerCase().startsWith("yesbot") &&
-    message.content.toLowerCase().endsWith("?")
-  )
-    randomReply(message);
-  if (
-    message.content.toLowerCase().includes("abooz") ||
-    message.content.toLowerCase().includes("mod abuse")
-  ) {
-    await message.react("👀");
-  }
 
   if (message.content.toLowerCase().startsWith("!group toggle"))
     await GroupManager(message, true);
