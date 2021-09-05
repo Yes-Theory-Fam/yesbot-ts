f05db54134d23e4d6e6bb93e4d1b481936964c6a
Hasan Latch <hassoune80@gmail.com>
2021-09-05 13:27:24 +0200

    requested changes

--- src/programs/productivity-toggle.ts
+++ src/programs/productivity-toggle.ts
@@ -1,5 +1,5 @@
 import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
-import { TextChannel, VoiceChannel, VoiceState, Message } from "discord.js";
+import { VoiceChannel, VoiceState, Message, GuildMember } from "discord.js";
 import { VoiceStateChange } from "../event-distribution/events/voice-state-update";
 import { ChatNames } from "../collections/chat-names";
 import Tools from "../common/tools";
@@ -8,8 +8,6 @@ import { hasRole } from "../common/moderator";
 @Command({
   event: DiscordEvent.VOICE_STATE_UPDATE,
   changes: [VoiceStateChange.JOINED, VoiceStateChange.SWITCHED_CHANNEL],
-  description:
-    "This handler checks if the user left one of the productivity channels",
 })
 class AddBreakRoleOnJoin
   implements CommandHandler<DiscordEvent.VOICE_STATE_UPDATE>
@@ -19,62 +17,13 @@ class AddBreakRoleOnJoin
 
     const channel = newState.channel;
 
-    if (
-      channel === null ||
-      (channel.name != ChatNames.PRODUCTIVITY &&
-        channel.name != ChatNames.WATCH_ME_WORK &&
-        channel.name != ChatNames.WATCH_ME_WORK_TOO)
-    )
-      return;
+    if (!isProdVoiceChannel(channel)) return;
 
-    const channelName = channel.name;
+    const guildMember = newState.member;
+    await createProductivityPermissions(guildMember, channel);
 
-    if (
-      channelName === ChatNames.PRODUCTIVITY ||
-      channelName === ChatNames.WATCH_ME_WORK ||
-      channelName === ChatNames.WATCH_ME_WORK_TOO
-    ) {
-      const watchMeWorkChannel = newState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.WATCH_ME_WORK
-      ) as VoiceChannel;
-      const watchMeWorkTooChannel = newState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.WATCH_ME_WORK_TOO
-      ) as VoiceChannel;
-      const ProductivityChannel = newState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.PRODUCTIVITY
-      ) as VoiceChannel;
-      const voiceChatWIPChannel = newState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.VOICE_CHAT_WIP
-      ) as TextChannel;
-
-      const guildMember = newState.member;
-
-      await watchMeWorkChannel.updateOverwrite(guildMember.id, {
-        VIEW_CHANNEL: true,
-        CONNECT: true,
-        STREAM: true,
-        USE_VAD: true,
-      });
-      await watchMeWorkTooChannel.updateOverwrite(guildMember.id, {
-        VIEW_CHANNEL: true,
-        CONNECT: true,
-        STREAM: true,
-        USE_VAD: true,
-      });
-      await ProductivityChannel.updateOverwrite(guildMember.id, {
-        CONNECT: true,
-        VIEW_CHANNEL: true,
-        SPEAK: false,
-      });
-      await voiceChatWIPChannel.updateOverwrite(guildMember.id, {
-        VIEW_CHANNEL: true,
-        SEND_MESSAGES: true,
-        READ_MESSAGE_HISTORY: true,
-      });
-
-      const breakRole = Tools.getRoleByName("Break", newState.guild);
-      await guildMember.roles.add(breakRole);
-    }
+    const breakRole = Tools.getRoleByName("Break", newState.guild);
+    await guildMember.roles.add(breakRole);
   }
 }
 
@@ -88,46 +37,15 @@ class RemoveBreakRoleOnLeave
   implements CommandHandler<DiscordEvent.VOICE_STATE_UPDATE>
 {
   async handle(oldState: VoiceState, newState: VoiceState): Promise<void> {
-    const channel = oldState.channel.name;
+    const channel = oldState.channel;
 
-    if (
-      channel === ChatNames.PRODUCTIVITY ||
-      channel === ChatNames.WATCH_ME_WORK ||
-      channel === ChatNames.WATCH_ME_WORK_TOO
-    ) {
-      const watchMeWorkChannel = oldState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.WATCH_ME_WORK
-      ) as VoiceChannel;
-      const watchMeWorkTooChannel = oldState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.WATCH_ME_WORK_TOO
-      ) as VoiceChannel;
-      const ProductivityChannel = oldState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.PRODUCTIVITY
-      ) as VoiceChannel;
-      const voiceChatWIPChannel = oldState.guild.channels.cache.find(
-        (channel) => channel.name === ChatNames.VOICE_CHAT_WIP
-      ) as TextChannel;
+    if (!isProdVoiceChannel(channel)) return;
 
-      const guildMember = oldState.member;
+    const guildMember = oldState.member;
+    await revertProductivityPermissions(guildMember, channel);
 
-      const breakRole = Tools.getRoleByName("Break", oldState.guild);
-      await guildMember.roles.remove(breakRole);
-
-      await watchMeWorkChannel.permissionOverwrites
-        .get(guildMember.id)
-        .delete();
-      await watchMeWorkTooChannel.permissionOverwrites
-        .get(guildMember.id)
-        .delete();
-      await ProductivityChannel.permissionOverwrites
-        .get(guildMember.id)
-        .delete();
-      await voiceChatWIPChannel.permissionOverwrites
-        .get(guildMember.id)
-        .delete();
-
-      return;
-    }
+    const breakRole = Tools.getRoleByName("Break", oldState.guild);
+    await guildMember.roles.remove(breakRole);
   }
 }
 
@@ -141,40 +59,20 @@ class ChannelSwitchCheck
   implements CommandHandler<DiscordEvent.VOICE_STATE_UPDATE>
 {
   async handle(oldState: VoiceState, newState: VoiceState): Promise<void> {
-    const oldChannelName = oldState.channel.name;
-    const newChannelName = newState.channel.name;
-
-    if (
-      ((oldChannelName === ChatNames.PRODUCTIVITY ||
-        oldChannelName === ChatNames.WATCH_ME_WORK ||
-        oldChannelName === ChatNames.WATCH_ME_WORK_TOO) &&
-        newChannelName === ChatNames.PRODUCTIVITY) ||
-      newChannelName === ChatNames.WATCH_ME_WORK ||
-      newChannelName === ChatNames.WATCH_ME_WORK_TOO
-    )
+    const oldChannel = oldState.channel;
+    const newChannel = newState.channel;
+    if (isProdVoiceChannel(oldChannel) && isProdVoiceChannel(newChannel))
       return;
 
-    const watchMeWorkChannel = oldState.guild.channels.cache.find(
-      (channel) => channel.name === ChatNames.WATCH_ME_WORK
-    ) as VoiceChannel;
-    const watchMeWorkTooChannel = oldState.guild.channels.cache.find(
-      (channel) => channel.name === ChatNames.WATCH_ME_WORK_TOO
-    ) as VoiceChannel;
-    const ProductivityChannel = oldState.guild.channels.cache.find(
-      (channel) => channel.name === ChatNames.PRODUCTIVITY
-    ) as VoiceChannel;
-    const voiceChatWIPChannel = oldState.guild.channels.cache.find(
-      (channel) => channel.name === ChatNames.VOICE_CHAT_WIP
-    ) as TextChannel;
+    if (!isProdVoiceChannel(oldChannel) && isProdVoiceChannel(newChannel))
+      return;
+
+    if (!isProdVoiceChannel(oldChannel) && !isProdVoiceChannel(newChannel))
+      return;
 
     const guildMember = newState.member;
 
-    await watchMeWorkChannel.permissionOverwrites.get(guildMember.id).delete();
-    await watchMeWorkTooChannel.permissionOverwrites
-      .get(guildMember.id)
-      .delete();
-    await ProductivityChannel.permissionOverwrites.get(guildMember.id).delete();
-    await voiceChatWIPChannel.permissionOverwrites.get(guildMember.id).delete();
+    await revertProductivityPermissions(guildMember, oldChannel);
   }
 }
 
@@ -189,12 +87,7 @@ class ToggleChannelsInProdVC implements CommandHandler<DiscordEvent.MESSAGE> {
   async handle(message: Message): Promise<void> {
     const guildMember = message.member;
 
-    if (
-      guildMember.voice.channel === null ||
-      (guildMember.voice.channel.name != ChatNames.WATCH_ME_WORK &&
-        guildMember.voice.channel.name != ChatNames.WATCH_ME_WORK_TOO &&
-        guildMember.voice.channel.name != ChatNames.PRODUCTIVITY)
-    ) {
+    if (!isProdVoiceChannel(guildMember.voice.channel)) {
       await Tools.handleUserError(
         message,
         "You're not in a productivity voice channel!"
@@ -215,3 +108,70 @@ class ToggleChannelsInProdVC implements CommandHandler<DiscordEvent.MESSAGE> {
     }
   }
 }
+
+const isProdVoiceChannel = (channel: VoiceChannel) => {
+  const prodVCNames = [
+    ChatNames.PRODUCTIVITY,
+    ChatNames.WATCH_ME_WORK,
+    ChatNames.WATCH_ME_WORK_TOO,
+  ];
+  if (channel === null) return;
+  return prodVCNames.includes(channel.name as ChatNames);
+};
+
+const revertProductivityPermissions = async (
+  member: GuildMember,
+  channel: VoiceChannel
+) => {
+  const prodVoiceChats = [
+    ChatNames.WATCH_ME_WORK,
+    ChatNames.WATCH_ME_WORK_TOO,
+    ChatNames.PRODUCTIVITY,
+    ChatNames.VOICE_CHAT_WIP,
+  ];
+  for (const chatName of prodVoiceChats) {
+    const ProductivityChannels = channel.guild.channels.cache.find(
+      (c) => c.name === chatName
+    );
+    await ProductivityChannels.permissionOverwrites.get(member.id).delete();
+  }
+};
+
+const createProductivityPermissions = async (
+  member: GuildMember,
+  channel: VoiceChannel
+) => {
+  const prodVoiceChats = [
+    ChatNames.WATCH_ME_WORK,
+    ChatNames.WATCH_ME_WORK_TOO,
+    ChatNames.PRODUCTIVITY,
+    ChatNames.VOICE_CHAT_WIP,
+  ];
+  for (const chatName of prodVoiceChats) {
+    const ProductivityChannel = channel.guild.channels.cache.find(
+      (c) => c.name === chatName
+    );
+    await ProductivityChannel.updateOverwrite(member.id, {
+      VIEW_CHANNEL: true,
+      CONNECT: true,
+      STREAM: true,
+      USE_VAD: true,
+    });
+
+    if (ProductivityChannel.name === ChatNames.PRODUCTIVITY) {
+      await ProductivityChannel.updateOverwrite(member.id, {
+        CONNECT: true,
+        VIEW_CHANNEL: true,
+        SPEAK: false,
+      });
+    }
+
+    if (ProductivityChannel.name === ChatNames.VOICE_CHAT_WIP) {
+      await ProductivityChannel.updateOverwrite(member.id, {
+        VIEW_CHANNEL: true,
+        SEND_MESSAGES: true,
+        READ_MESSAGE_HISTORY: true,
+      });
+    }
+  }
+};
