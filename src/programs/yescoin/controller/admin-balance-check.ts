88449dd79edac6b9137624f271921174206da3a6
Adrian Görisch <adrian.goerisch@newcubator.com>
2021-10-12 21:09:18 +0200

    feat(currency): request balance (#526)
    * feat(currency): request balance
    
    * admins are able to request the balance of any user in
    bot-development.
    * users are able to request thir own balance via DMs
    
    * feat(currency): request balance
    
    * resolve comments

---
+++ src/programs/yescoin/controller/admin-balance-check.ts
@@ -0,0 +1,31 @@
+import { Message } from "discord.js";
+import {
+  Command,
+  CommandHandler,
+  DiscordEvent,
+} from "../../../event-distribution";
+import { ChatNames } from "../../../collections/chat-names";
+import { GetBalance } from "../usecase/get-balance";
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  allowedRoles: ["Support", "Developer"],
+  channelNames: [ChatNames.BOT_DEVELOPMENT],
+  trigger: "!yescoin-status",
+  description: "This handler return the balance of mentioned users",
+})
+export class AdminBalanceCheck implements CommandHandler<DiscordEvent.MESSAGE> {
+  public async handle(arg: Message): Promise<void> {
+    const member = arg.mentions.users.first() ?? arg.author;
+
+    await GetBalance.instance()
+      .handle(member.id)
+      .then((value) => {
+        arg.channel.send(
+          `<@${member.id}> has currently ${value}${arg.client.emojis.cache.find(
+            (value) => value.name === "yescoin"
+          )}`
+        );
+      });
+  }
+}
