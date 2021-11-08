a2cce5832db019b8fb6050b0dc86542579e3c0af
Hasan Latch <hassoune80@gmail.com>
2021-11-08 17:42:21 +0100

    added ability to add reactions to any messages and edit yesbot messages

---
+++ src/programs/remote-control/edit.ts
@@ -0,0 +1,61 @@
+import { Message, TextChannel } from "discord.js";
+import bot from "../..";
+import Tools from "../../common/tools";
+import {
+  Command,
+  CommandHandler,
+  DiscordEvent,
+} from "../../event-distribution";
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  trigger: "!message",
+  subTrigger: "edit",
+  allowedRoles: ["Support"],
+  description: "This command lets you edit messages send by Yesbot",
+})
+class EditMessage implements CommandHandler<DiscordEvent.MESSAGE> {
+  async handle(message: Message): Promise<void> {
+    const [, , channelId, messageId] = message.content.split(" ");
+
+    if (!channelId || !messageId) {
+      await Tools.handleUserError(
+        message,
+        "Missing channelId or messageId. Syntax: `!message edit channelId messageId"
+      );
+      return;
+    }
+
+    const channel = bot.channels.resolve(channelId) as TextChannel;
+    const messageToEdit = await channel.messages.fetch(messageId);
+
+    if (!channel || !messageToEdit) {
+      await Tools.handleUserError(
+        message,
+        "I could not find that channel or message! Please verify the arguments given"
+      );
+      return;
+    }
+
+    const requestMessage = await message.channel.send({
+      content:
+        "Okay, now all you need to do is copy the original message and edit it and send it here and I will take care of the rest! :yesbot:",
+    });
+
+    try {
+      const editMessage = await message.channel.awaitMessages({
+        filter: (m) => !m.author.bot && m.author == message.author,
+        time: 60000,
+        max: 1,
+      });
+
+      const requestedEdit = editMessage.first().content;
+      await messageToEdit.edit(requestedEdit);
+      await editMessage.first().delete();
+      await requestMessage.delete();
+      await message.react("👍");
+    } catch (err) {
+      await message.react("👎");
+    }
+  }
+}
--- src/programs/remote-control/send.ts
+++ src/programs/remote-control/send.ts
@@ -1,40 +1,54 @@
 import { Message, TextChannel, Util } from "discord.js";
 import bot from "../..";
 import Tools from "../../common/tools";
-import { Command, CommandHandler, DiscordEvent } from "../../event-distribution";
-
-
-
+import {
+  Command,
+  CommandHandler,
+  DiscordEvent,
+} from "../../event-distribution";
 
 @Command({
-    event: DiscordEvent.MESSAGE,
-    trigger: "!message",
-    subTrigger: "send",
-    allowedRoles: ["Support"],
-    description: "This allows you to send messages using yesbot!"
+  event: DiscordEvent.MESSAGE,
+  trigger: "!message",
+  subTrigger: "send",
+  allowedRoles: ["Support"],
+  description: "This allows you to send messages using yesbot!",
 })
 class SendMessage implements CommandHandler<DiscordEvent.MESSAGE> {
-    async handle(message: Message): Promise<void> {
-        const words = message.content.split(" ")
-        const channelId = words[2]
-        const messageToSend = words.slice(3).join(" ")
+  async handle(message: Message): Promise<void> {
+    const words = message.content.split(" ");
+    const channelId = words[2];
+    const messageToSend = words.slice(3).join(" ");
 
-        if (!channelId || !messageToSend) {
-            await Tools.handleUserError(message, "Missing channelId or message to send. Syntax: `!message send {channelId} {message content}`")
-            return;
-        }
-
-        const channel = bot.channels.resolve(channelId) as TextChannel
-
-        if (!channel) {
-            await Tools.handleUserError(message, "I could not find that channel! Verify the channelId")
-            return;
-        }
-
-        const messagesBatches = Util.splitMessage(messageToSend, {char: " "})
-        for (const batch of messagesBatches) {
-            await channel.send({content: batch})
-        }
-        await message.delete()
+    if (!channelId || !messageToSend) {
+      await Tools.handleUserError(
+        message,
+        "Missing channelId or message to send. Syntax: `!message send {channelId} {message content}`"
+      );
+      return;
     }
+
+    const channel = bot.channels.resolve(channelId) as TextChannel;
+
+    if (!channel) {
+      await Tools.handleUserError(
+        message,
+        "I could not find that channel! Verify the channelId"
+      );
+      return;
+    }
+
+    try {
+      const messagesBatches = Util.splitMessage(messageToSend, { char: " " });
+      for (const batch of messagesBatches) {
+        await channel.send({ content: batch });
+      }
+
+      await message.delete();
+    } catch (err) {
+      await message.reply(
+        "I seem to had a little hiccup while sending custom messages please verify I didn't send anything by mistake :c"
+      );
+    }
+  }
 }
