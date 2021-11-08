98c5b5fd20aecc52fd5b96b46809e10ce25dafd6
Hasan Latch <hassoune80@gmail.com>
2021-11-08 10:43:51 +0100

    WIP: added ability to send custom messages

---
+++ src/programs/remote-control/send.ts
@@ -0,0 +1,40 @@
+import { Message, TextChannel, Util } from "discord.js";
+import bot from "../..";
+import Tools from "../../common/tools";
+import { Command, CommandHandler, DiscordEvent } from "../../event-distribution";
+
+
+
+
+@Command({
+    event: DiscordEvent.MESSAGE,
+    trigger: "!message",
+    subTrigger: "send",
+    allowedRoles: ["Support"],
+    description: "This allows you to send messages using yesbot!"
+})
+class SendMessage implements CommandHandler<DiscordEvent.MESSAGE> {
+    async handle(message: Message): Promise<void> {
+        const words = message.content.split(" ")
+        const channelId = words[2]
+        const messageToSend = words.slice(3).join(" ")
+
+        if (!channelId || !messageToSend) {
+            await Tools.handleUserError(message, "Missing channelId or message to send. Syntax: `!message send {channelId} {message content}`")
+            return;
+        }
+
+        const channel = bot.channels.resolve(channelId) as TextChannel
+
+        if (!channel) {
+            await Tools.handleUserError(message, "I could not find that channel! Verify the channelId")
+            return;
+        }
+
+        const messagesBatches = Util.splitMessage(messageToSend, {char: " "})
+        for (const batch of messagesBatches) {
+            await channel.send({content: batch})
+        }
+        await message.delete()
+    }
+}
