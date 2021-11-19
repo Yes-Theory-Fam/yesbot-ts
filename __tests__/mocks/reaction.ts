f0ceb32f07732049ca278cabe443d094e1abf7da
geisterfurz007 <michelvonv@me.com>
2021-11-19 17:43:50 +0100

    fix: discord.js test typescript

--- __tests__/mocks/reaction.ts
+++ __tests__/mocks/reaction.ts
@@ -1,6 +1,7 @@
 import { Client, GuildMember, Message, MessageReaction } from "discord.js";
 import { RawMessageReactionData } from "discord.js/typings/rawDataTypes";
 
+// @ts-expect-error: https://github.com/discordjs/discord.js/issues/6798
 export class MockMessageReaction extends MessageReaction {
   constructor(
     member: GuildMember,
