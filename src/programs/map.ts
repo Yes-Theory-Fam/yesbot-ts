b2f99194fc942f3590917f2c17bb1ba60187b24e
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-10-07 00:29:24 +0530

    feat: convert map commands to slash command (#822)
    * convert map commands to slash command
    
    * remove unwanted fields, rephrase descriptions

--- src/programs/map.ts
+++ src/programs/map.ts
@@ -1,68 +1,77 @@
-import { Message, EmbedBuilder } from "discord.js";
-import Tools from "../common/tools";
-import { CountryRoleFinder } from "../common/country-role-finder";
+import {
+  ApplicationCommandOptionType,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+} from "discord.js";
 import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
-import { ChatNames } from "../collections/chat-names";
+import { CountryRoleFinder } from "../common/country-role-finder";
 
 @Command({
-  event: DiscordEvent.MESSAGE,
-  trigger: "!map",
-  channelNames: [ChatNames.BOT_COMMANDS],
-  description: "This handler is to manage the map command",
+  event: DiscordEvent.SLASH_COMMAND,
+  root: "map",
+  subCommand: "show",
+  description: "Show a map of Yes Fam members' locations!",
 })
-class Map implements CommandHandler<DiscordEvent.MESSAGE> {
-  async handle(message: Message) {
-    await message.reply(
-      `you can find the message link here: ${process.env.MAP_LINK} \nIf you want your city to be added to it, type !mapadd [city, country]`
-    );
+class Map implements CommandHandler<DiscordEvent.SLASH_COMMAND> {
+  async handle(interaction: ChatInputCommandInteraction) {
+    await interaction.reply({
+      content: `You can find the message link here: ${process.env.MAP_LINK} \nIf you want your city to be added to it, use </map add:${interaction.commandId}> command`,
+      ephemeral: true,
+    });
   }
 }
 
 @Command({
-  event: DiscordEvent.MESSAGE,
-  trigger: "!mapadd",
-  channelNames: [ChatNames.BOT_COMMANDS],
-  description: "This handler is to manage the mapadd command",
+  event: DiscordEvent.SLASH_COMMAND,
+  root: "map",
+  subCommand: "add",
+  description: "Add your location to the map!",
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "city",
+      description: "Name of your city",
+      required: true,
+    },
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "country",
+      description: "Name of your country",
+    },
+  ],
 })
-class MapAdd implements CommandHandler<DiscordEvent.MESSAGE> {
-  async handle(message: Message) {
-    const split = message.content.split(" ");
-    split.shift();
-    const city = split.join(" ");
-    if (!city) {
-      await Tools.handleUserError(
-        message,
-        "You need to add the city you are from!"
-      );
-      return;
-    }
+class MapAdd implements CommandHandler<DiscordEvent.SLASH_COMMAND> {
+  async handle(interaction: ChatInputCommandInteraction) {
+    const city = interaction.options.getString("city");
+    const country = interaction.options.getString("country");
+    const member = await interaction.guild?.members.fetch(interaction.user.id);
 
-    const countries = message.member?.roles.cache
+    const countries = member?.roles.cache
       .filter((role) => CountryRoleFinder.isCountryRole(role.name))
       .map((role) => CountryRoleFinder.getCountryByRole(role.name));
 
-    const mapMaintainerDm = await message.guild?.members
+    const mapMaintainerDm = await interaction.guild?.members
       .resolve(process.env.MAP_ADD_DM_USER_ID)
       ?.user.createDM();
-    const author = message.member;
 
     const dmEmbed = new EmbedBuilder()
-      .setTitle(`Map Update Requested`)
+      .setTitle("Map Update Requested")
       .setFields([
-        { name: "UserID:", value: message.author.id },
+        { name: "UserID:", value: interaction.user.id },
         {
-          name: "Current Name on the server:",
-          value: author?.displayName ?? "",
+          name: "Current Name on the Server:",
+          value: member?.displayName ?? "",
         },
-        { name: "Current Discord Tag:", value: author?.user.tag ?? "" },
-        { name: "City / Location:", value: city },
-        { name: "Countries:", value: countries?.join(",") ?? "" },
-        { name: "Link to the message:", value: `[here](${message.url})` },
+        { name: "Current Discord Tag:", value: interaction.user.tag },
+        { name: "City:", value: city ?? "" },
+        { name: "Countries:", value: (country || countries?.join(",")) ?? "" },
       ]);
 
     await mapMaintainerDm?.send({ embeds: [dmEmbed] });
-    await message.reply(
-      "I messaged the maintainer of the map, they will add you to it soon!"
-    );
+    await interaction.reply({
+      content:
+        "I messaged the maintainer of the map, they will add you to it soon!",
+      ephemeral: true,
+    });
   }
 }
