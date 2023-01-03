5593c5773b31065cbc955d03d534fd0ed4982672
geisterfurz007 <michelvonv@me.com>
2023-01-03 16:44:04 +0100

    feat: return Buddy Project role to matched members on rejoin

---
+++ src/programs/buddy-project/sign-up/buddy-project-join.ts
@@ -0,0 +1,26 @@
+import { GuildMember } from "discord.js";
+import { BuddyProjectStatus } from "../../../__generated__/types";
+import Tools from "../../../common/tools";
+import {
+  Command,
+  CommandHandler,
+  DiscordEvent,
+} from "../../../event-distribution";
+import { BuddyProjectService } from "../services/buddy-project.service";
+
+@Command({
+  event: DiscordEvent.MEMBER_JOIN,
+})
+class BuddyProjectJoin extends CommandHandler<DiscordEvent.MEMBER_JOIN> {
+  async handle(member: GuildMember): Promise<void> {
+    const buddyProjectService = new BuddyProjectService();
+    const status = await buddyProjectService.getBuddy(member.id);
+
+    if (status.status === BuddyProjectStatus.Matched) {
+      const buddyProjectRoleName = `Buddy Project ${new Date().getFullYear()}`;
+      const role = Tools.getRoleByName(buddyProjectRoleName, member.guild);
+
+      if (role) await member.roles.add(role);
+    }
+  }
+}
