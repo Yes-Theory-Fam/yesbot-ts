616c809b0b7e9f856a6e1ebdb74e9aa6590dc262
geisterfurz007 <michelvonv@me.com>
2023-01-22 14:38:48 +0100

    chore: remove timed_out_users + users_on_break tables

---
+++ prisma/migrations/20230122133711_remove_unused_tables/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `timed_out_users` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users_on_break` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "timed_out_users";
+
+-- DropTable
+DROP TABLE "users_on_break";
