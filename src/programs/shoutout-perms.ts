ef38c40106661f7cdc94223ba3053d60f5912689
Christolis <work@chris-sdogkos.com>
2023-03-26 13:26:04 +0300

    shoutout permission now gets revoked after a day!
    Overall, this system creates a timer for each individual user whenever they
    get the permission to send messages to the "shoutouts" text channel. Below
    are the conditions for when a priviliged user loses their permission and
    their timer gets cancelled:
    - They send a message to the shoutouts channel.
    - 24 hours pass without a message sent to the channel.
    - A person who is authorized to use the shoutouts command toggles the
      permission for them to send messages.

--- src/programs/shoutout-perms.ts
+++ src/programs/shoutout-perms.ts
@@ -1,15 +1,30 @@
 import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
 import {
   ChannelType,
+  Client,
   ChatInputCommandInteraction,
+  Guild,
+  Snowflake,
   OverwriteType,
   TextChannel,
   ApplicationCommandOptionType,
+  PermissionOverwrites,
   Message,
   User,
 } from "discord.js";
 import bot from "..";
 import { ChatNames } from "../collections/chat-names";
+import { createYesBotLogger } from "../log";
+import { Timer } from "@prisma/client";
+import { TimerService } from "./timer/timer.service";
+
+interface ShoutoutPermsData {
+  channelId: Snowflake;
+  userId: Snowflake;
+}
+
+const shoutoutPermsIdentifier = "shoutoutPermsRemove";
+const shoutoutTimers: Record<Snowflake, string> = {};
 
 const getSendPerms = (channel: TextChannel, user: User) => {
   return channel.permissionOverwrites.cache
@@ -17,6 +32,24 @@ const getSendPerms = (channel: TextChannel, user: User) => {
     .first();
 };
 
+const getShoutoutsChannel = (guild: Guild) => {
+  return guild.channels.cache
+    .filter(
+      (c) => c.isTextBased() && (c as TextChannel).name === ChatNames.SHOUTOUTS
+    )
+    .first();
+};
+
+const deletePerms = async (
+  userId: Snowflake,
+  sendPerms: PermissionOverwrites
+) => {
+  const timerId = shoutoutTimers[userId];
+  await sendPerms.delete();
+
+  delete shoutoutTimers[userId];
+  if (timerId !== null) TimerService.cancelTimer(timerId);
+};
 @Command({
   event: DiscordEvent.SLASH_COMMAND,
   root: "shoutout-perms",
@@ -58,12 +91,7 @@ class ShoutoutPermsToggleCommand
     }
 
     // Get the shoutouts channel
-    const shoutouts = interaction.guild.channels.cache
-      .filter(
-        (c) =>
-          c.isTextBased() && (c as TextChannel).name === ChatNames.SHOUTOUTS
-      )
-      .first();
+    const shoutouts = getShoutoutsChannel(interaction.guild);
 
     // there must be a way to type this in a more typescript-ish way
     if (shoutouts === null || shoutouts?.type !== ChannelType.GuildText) {
@@ -74,10 +102,13 @@ class ShoutoutPermsToggleCommand
       return;
     }
 
+    // Check whether there's an instance
     // If there is an existing perm overwrite, remove it
     const sendPerms = getSendPerms(shoutouts, user);
     if (sendPerms && getSendPerms(shoutouts, user)) {
-      await sendPerms.delete();
+      deletePerms(user.id, sendPerms);
+
+      // TODO: Make this message a bit nicer!
       await interaction.reply({
         content: "Removed the permission!",
         ephemeral: true,
@@ -93,6 +124,20 @@ class ShoutoutPermsToggleCommand
       content: "Successfully gave the permission!",
       ephemeral: true,
     });
+
+    // Create the timer (runs in 1 day)
+    const executeTime = new Date();
+    executeTime.setDate(executeTime.getDate() + 1);
+    const timerId = await TimerService.createTimer(
+      shoutoutPermsIdentifier,
+      executeTime,
+      {
+        channelId: shoutouts.id,
+        userId: user.id,
+      }
+    );
+    shoutoutTimers[user.id] = timerId;
+
     await channel.send({
       content: `Hey <@${user.id}>, feel free to send a message on the <#${shoutouts.id}> channel!`,
     });
@@ -118,8 +163,25 @@ class ShoutoutMessageHandlerCommand
 
     const sendPerms = getSendPerms(channel, sender);
 
-    if (sendPerms) {
-      await sendPerms.delete();
-    }
+    if (sendPerms) await deletePerms(sender.id, sendPerms);
   }
 }
+
+@Command({
+  event: DiscordEvent.TIMER,
+  handlerIdentifier: shoutoutPermsIdentifier,
+})
+class ShoutoutTimer extends CommandHandler<DiscordEvent.TIMER> {
+  async handle(timer: Timer): Promise<void> {
+    const data = timer.data as unknown as ShoutoutPermsData;
+    const channel = bot.channels.resolve(data.channelId) as TextChannel;
+    const user = bot.users.resolve(data.userId);
+
+    if (user === null || channel === null) return;
+    const sendPerms = getSendPerms(channel, user);
+
+    if (sendPerms) await deletePerms(user.id, sendPerms);
+  }
+}
+
+const logger = createYesBotLogger("shoutouts", ShoutoutTimer.name);
