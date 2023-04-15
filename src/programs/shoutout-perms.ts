4d2b8bc3a667525b897a54f3c10cc589f4ccf181
Chris Sdogkos <work@chris-sdogkos.com>
2023-04-15 23:41:30 +0300

    Approve shoutout tickets (#966)
    * add initial shoutout-approve command code
    
    * switch from two shoutout commands to one toggle command
    
    * remove unused import
    
    * improve messages and make the cmd usable only in shoutout tickets
    
    * rename approve-shoutout.ts to shoutout-perms.ts
    
    * use ChatNames for the shoutouts channel instead of keeping a reference to it locally
    
    * automatically remove permission when user posts
    
    * shoutout permission now gets revoked after a day!
    
    Overall, this system creates a timer for each individual user whenever they
    get the permission to send messages to the "shoutouts" text channel. Below
    are the conditions for when a priviliged user loses their permission and
    their timer gets cancelled:
    - They send a message to the shoutouts channel.
    - 24 hours pass without a message sent to the channel.
    - A person who is authorized to use the shoutouts command toggles the
      permission for them to send messages.
    
    * tickets now use ids instead of the name and tag
    
    The command "/shoutout-perms" now does not require an argument of the
    user in order to function. It uses the ID contained in the channel's
    name to figure out which user to toggle the shoutout permissions for.
    
    Keep in mind that the change to userIds also applies to other types of
    tickets!
    
    * clean up code
    
    * remove unused imports
    
    * remove unnecessary condition
    
    * fix lint error
    
    * implement changes from review
    
    - Shoutout timers now don't get cancelled even if someone revokes the
      permissions earlier than a day.
    - Unconditionally delete the permission overwrites, without checking on
      whether they exist or not.
    - Improve the message sent to the user if they enter the command at the
      wrong channel.
    - Remove unused logger instance.
    
    * forgot to run prettier
    
    * Update src/programs/shoutout-perms.ts
    
    Co-authored-by: geisterfurz007 <geisterfurz007@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: geisterfurz007 <geisterfurz007@users.noreply.github.com>

---
+++ src/programs/shoutout-perms.ts
@@ -0,0 +1,155 @@
+import { Command, CommandHandler, DiscordEvent } from "../event-distribution";
+import {
+  ChannelType,
+  ChatInputCommandInteraction,
+  Guild,
+  Snowflake,
+  OverwriteType,
+  TextChannel,
+  PermissionOverwrites,
+  PermissionsBitField,
+  Message,
+  User,
+} from "discord.js";
+import bot from "..";
+import { ChatNames } from "../collections/chat-names";
+import { createYesBotLogger } from "../log";
+import { Timer } from "@prisma/client";
+import { TimerService } from "./timer/timer.service";
+
+interface ShoutoutPermsData {
+  channelId: Snowflake;
+  userId: Snowflake;
+}
+
+const shoutoutPermsIdentifier = "shoutoutPermsRemove";
+
+const getShoutoutsChannel = (guild: Guild) => {
+  return guild.channels.cache
+    .filter(
+      (c) => c.isTextBased() && (c as TextChannel).name === ChatNames.SHOUTOUTS
+    )
+    .first();
+};
+
+@Command({
+  event: DiscordEvent.SLASH_COMMAND,
+  root: "shoutout-perms",
+  description: "Manage who can post in the #shoutout channel!",
+  subCommand: "toggle",
+})
+class ShoutoutPermsToggleCommand
+  implements CommandHandler<DiscordEvent.SLASH_COMMAND>
+{
+  async handle(interaction: ChatInputCommandInteraction): Promise<void> {
+    if (!interaction.guild) return;
+    const channel = interaction.channel;
+
+    if (channel?.type !== ChannelType.GuildText) return;
+    if (!channel.name.startsWith("shoutout-")) {
+      await interaction.reply({
+        content: "This command must be used in a shoutout ticket's channel!",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Get handle of the user
+    const user = interaction.client.users.resolve(channel.name.split("-")[1]);
+    if (user === null) {
+      await interaction.reply({
+        content: "Couldn't find the member!",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Get the shoutouts channel
+    const shoutouts = getShoutoutsChannel(interaction.guild);
+    if (shoutouts?.type !== ChannelType.GuildText) {
+      await interaction.reply({
+        content: `Couldn't find the <#${ChatNames.SHOUTOUTS}> channel!`,
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Check whether there's an instance.
+    // If there is an existing perm overwrite, remove it
+    const perms = await shoutouts.permissionsFor(user);
+    const hasSendPerms =
+      perms?.has(PermissionsBitField.Flags.SendMessages, false) ?? false;
+
+    if (hasSendPerms) {
+      await shoutouts.permissionOverwrites.delete(user);
+
+      await interaction.reply({
+        content: "Removed the permission!",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Otherwise, create a new one
+    await shoutouts.permissionOverwrites.edit(user.id, {
+      SendMessages: true,
+    });
+    await interaction.reply({
+      content: "Successfully gave the permission!",
+      ephemeral: true,
+    });
+
+    // Create the timer (runs in 1 day)
+    const executeTime = new Date();
+    executeTime.setDate(executeTime.getDate() + 1);
+    const timerId = await TimerService.createTimer(
+      shoutoutPermsIdentifier,
+      executeTime,
+      {
+        channelId: shoutouts.id,
+        userId: user.id,
+      }
+    );
+
+    await channel.send({
+      content: `Hey <@${user.id}>, feel free to send a message on the <#${shoutouts.id}> channel!`,
+    });
+  }
+}
+
+@Command({
+  event: DiscordEvent.MESSAGE,
+  description:
+    "This handler removes the user's permission to send messages in the shoutout channel once used",
+  channelNames: [ChatNames.SHOUTOUTS],
+})
+class ShoutoutMessageHandlerCommand
+  implements CommandHandler<DiscordEvent.MESSAGE>
+{
+  async handle(message: Message): Promise<void> {
+    const sender = message.member?.user;
+    const channel = message.channel;
+
+    if (!sender || !channel) return;
+    if (sender.id === bot.user?.id) return;
+    if (channel.type !== ChannelType.GuildText) return;
+
+    await channel.permissionOverwrites.delete(sender);
+  }
+}
+
+@Command({
+  event: DiscordEvent.TIMER,
+  handlerIdentifier: shoutoutPermsIdentifier,
+})
+class ShoutoutTimer extends CommandHandler<DiscordEvent.TIMER> {
+  async handle(timer: Timer): Promise<void> {
+    const data = timer.data as unknown as ShoutoutPermsData;
+    const channel = bot.channels.resolve(data.channelId) as TextChannel;
+    const user = bot.users.resolve(data.userId);
+
+    if (!user || !channel) return;
+
+    await channel.permissionOverwrites.delete(user);
+  }
+}
