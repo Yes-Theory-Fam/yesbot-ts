9c12bddc3b2e6413fbad470ef3285c1ddfceff89
geisterfurz007 <michelvonv@me.com>
2023-06-25 15:58:00 +0200

    chore: fix env typing for member_role_id

--- environment.d.ts
+++ environment.d.ts
@@ -12,7 +12,7 @@ declare namespace NodeJS {
     MAP_ADD_DM_USER_ID: string;
     MODERATOR_ROLE_NAME: string;
     ENGINEER_ROLE_NAME: string;
-    MEMBER_ROLE_ID?: string;
+    MEMBER_ROLE_ID: string;
     GITHUB_TOKEN: string;
     GITHUB_GRAPHQL_ENDPOINT: string;
     GOOGLE_API_KEY?: string;
