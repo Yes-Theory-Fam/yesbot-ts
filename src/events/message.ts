d183da826bc855d5950bc4c28dc18061094c531f
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2023-07-14 22:47:37 +0200

    feat: fallback message for old commands (#1017)
    * feat: fallback message for old commands
    
    * chore: formatting

--- src/events/message.ts
+++ src/events/message.ts
@@ -23,8 +23,19 @@ const commandMap: Record<string, string> = {
   "@group": "group ping",
 };
 
+const oldContextMenuCommands: string[] = ["!meta", "!addvote"];
+
 export const legacyCommandHandler = async (message: Message) => {
-  const firstWord = message.content.split(" ")[0];
+  const firstWord = message.content.split(" ")[0].toLowerCase();
+
+  if (oldContextMenuCommands.includes(firstWord)) {
+    await Tools.handleUserError(
+      message,
+      "Hey there, it seems you have used one of the commands that have been converted to context menu commands. You can select the message now and go to Apps to make use of the command instead"
+    );
+    return;
+  }
+
   const newCommand = commandMap[firstWord];
 
   if (!newCommand) return;
